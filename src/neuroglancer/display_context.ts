google/neuroglancer

commit 2f07d18f821417f286be0a8445b4db53cda6d6ea

fix: disable alpha channel in canvas's framebuffer
The alpha channel of the canvas's framebuffer allows blending with other elements in the browser,
but this functionality is not needed by Neuroglancer, and was leading to inconsistent results on
different browsers.

src/neuroglancer/webgl/context.ts
@@ -31,6 +31,7 @@ export function initializeWebGL(canvas: HTMLCanvasElement) {
   let options: any = {
     'antialias': false,
     'stencil': true,
+    'alpha': false,
   };
   if (DEBUG_SHADERS) {
     console.log('DEBUGGING via preserveDrawingBuffer');

commit facc07703309c0c2371d2217809e9b0ff71f189e

fix: clear alpha channel to 1 after drawing
This corrects the regression introduced by 2f07d18f821417f286be0a8445b4db53cda6d6ea.

WebGL contexts created with alpha support disabled for the primary framebuffer appear to result in
flickering and other rendering issues on Mac OS.  To work around this problem, the alpha channel is
left enabled, but is cleared to 1 after drawing.

src/neuroglancer/display_context.ts
@@ -149,6 +149,13 @@ export class DisplayContext extends RefCounted {
         panel.setGLViewport();
         panel.draw();
       }
+
+      // Ensure the alpha buffer is set to 1.
+      gl.disable(gl.SCISSOR_TEST);
+      this.gl.clearColor(1.0, 1.0, 1.0, 1.0);
+      this.gl.colorMask(false, false, false, true);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      this.gl.colorMask(true, true, true, true);
     }
     this.updateFinished.dispatch();
   }

src/neuroglancer/webgl/context.ts
@@ -31,7 +31,6 @@ export function initializeWebGL(canvas: HTMLCanvasElement) {
   let options: any = {
     'antialias': false,
     'stencil': true,
-    'alpha': false,
   };
   if (DEBUG_SHADERS) {
     console.log('DEBUGGING via preserveDrawingBuffer');