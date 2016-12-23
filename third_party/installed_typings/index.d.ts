c3fe59f49e9e555222b79e099441864a9b9b80e9
Jeremy Maitin-Shepard <jbms@google.com>
2016-12-22 17:28:48 -0800

    chore: depend on @types/signals

--- third_party/installed_typings/index.d.ts
+++ third_party/installed_typings/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="globals/js-signals/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/webpack-env/index.d.ts" />
 /// <reference path="modules/gl-matrix/index.d.ts" />
