google/neuroglancer

commit c21f6919946ba60909f434dfb74098a5b09f0c5f

fix: improve sliceview multi-scale chunk priority formula
Previously, if for layer A, there was only a single high resolution scale, while for layer B, there
was both a high resolution and low resolution scale, then the high resolution scale of A would have
the same priority as the low resolution scale of B.

Under the new formula, the high resolution scale of A will have the same priority as the high
resolution scale of B, which will be lower than the priority of the low resolution scale of B.

src/neuroglancer/sliceview/backend.ts
@@ -72,7 +72,7 @@ export class SliceView extends SliceViewBase {
         let priorityIndex = sources.get(source);
         let chunk = source.getChunk(positionInChunks);
         chunkManager.requestChunk(
-            chunk, ChunkPriorityTier.VISIBLE, priority - SCALE_PRIORITY_MULTIPLIER * priorityIndex);
+            chunk, ChunkPriorityTier.VISIBLE, priority + SCALE_PRIORITY_MULTIPLIER * priorityIndex);
       }
     }
     this.computeVisibleChunks(getLayoutObject, addChunk);

src/neuroglancer/sliceview/base.ts
@@ -174,6 +174,8 @@ export class SliceViewBase extends SharedObject {
 
   /**
    * For each visible ChunkLayout, maps each visible VolumeChunkSource to its priority index.
+   * Overall chunk priority ordering is based on a lexicographical ordering of (priorityIndex,
+   * -distanceToCenter).
    */
   visibleChunkLayouts = new Map<ChunkLayout, Map<VolumeChunkSource, number>>();
 
@@ -321,7 +323,7 @@ export class SliceViewBase extends SharedObject {
           existingSources = new Map<VolumeChunkSource, number>();
           visibleChunkLayouts.set(chunkLayout, existingSources);
         }
-        existingSources.set(source, numSources - sourceScaleIndex - 1);
+        existingSources.set(source, sourceScaleIndex);
       };
 
       scaleIndex = numSources - 1;

commit 2c41653a7e659263d4e7fcf958e1fc7ef7b2849c

fix(sliceview): tweak sliceview chunk priority formula again
Fixes the problem introduced by commit c21f6919946ba60909f434dfb74098a5b09f0c5f of mesh/skeleton
chunks having lower priority than sliceview chunks.

src/neuroglancer/sliceview/backend.ts
@@ -22,7 +22,8 @@ import {vec3, vec3Key} from 'neuroglancer/util/geom';
 import {registerRPC, registerSharedObject, RPC, SharedObjectCounterpart} from 'neuroglancer/worker_rpc';
 import {Signal} from 'signals';
 
-const SCALE_PRIORITY_MULTIPLIER = 1e5;
+const BASE_PRIORITY = -1e12;
+const SCALE_PRIORITY_MULTIPLIER = 1e9;
 
 // Temporary values used by SliceView.updateVisibleChunk and VolumeChunkSource.computeChunkPosition.
 const tempChunkPosition = vec3.create();
@@ -72,7 +73,7 @@ export class SliceView extends SliceViewBase {
         let priorityIndex = sources.get(source);
         let chunk = source.getChunk(positionInChunks);
         chunkManager.requestChunk(
-            chunk, ChunkPriorityTier.VISIBLE, priority + SCALE_PRIORITY_MULTIPLIER * priorityIndex);
+            chunk, ChunkPriorityTier.VISIBLE, BASE_PRIORITY + priority + SCALE_PRIORITY_MULTIPLIER * priorityIndex);
       }
     }
     this.computeVisibleChunks(getLayoutObject, addChunk);