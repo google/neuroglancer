google/neuroglancer

commit 7ccd046c98fecfd650539426a9ec96caa34f5c75

Refactor: Move source management renderlayer code into generic sliceview class.
The backend renderlayer classes in point (vectorgraphics) and volume simply override the sources member and use the default SliceView RenderLayer constructor. The frontend classes work similarly. In addition, some volume specific code was removed from Volume/RenderLayer and placed in SliceView/RenderLayer

src/neuroglancer/sliceview/volume/renderlayer.ts
@@ -29,7 +29,6 @@ import {RenderLayer as GenericRenderLayer} from 'neuroglancer/layer';
 import {SLICEVIEW_RENDERLAYER_RPC_ID} from 'neuroglancer/sliceview/base';
 import {SliceView} from 'neuroglancer/sliceview/frontend';
 import {RenderLayer as GenericSliceViewRenderLayer} from 'neuroglancer/sliceview/renderlayer';
-import {SliceViewShaderBuffers} from 'neuroglancer/sliceview/renderlayer';
 import {VOLUME_RENDERLAYER_RPC_ID, VolumeChunkSpecification, VolumeSourceOptions} from 'neuroglancer/sliceview/volume/base';
 import {MultiscaleVolumeChunkSource, VolumeChunkSource} from 'neuroglancer/sliceview/volume/frontend';
 import {RefCounted} from 'neuroglancer/util/disposable';
@@ -79,6 +78,95 @@ vec3 getPositionWithinChunk () {
 const tempVec3 = vec3.create();
 const tempVec3b = vec3.create();
 const tempMat4 = mat4.create();
+
+export class SliceViewShaderBuffers extends RefCounted {
+  outputVertexIndices: Buffer;
+  vertexBasePositions: number[];
+  vertexIndices: Int32Array;
+  constructor(gl: GL) {
+    super();
+    this.outputVertexIndices = this.registerDisposer(
+        Buffer.fromData(gl, new Float32Array([0, 1, 2, 3, 4, 5]), gl.ARRAY_BUFFER, gl.STATIC_DRAW));
+
+    // This specifies the original, "uncorrected" vertex positions.
+    // var vertexBasePositions = [
+    //   0, 0, 0,
+    //   1, 0, 0,
+    //   0, 1, 0,
+    //   0, 0, 1,
+    //   1, 0, 1,
+    //   1, 1, 0,
+    //   0, 1, 1,
+    //   1, 1, 1,
+    // ];
+
+    // This specifies the "corrected" vertex positions.
+    this.vertexBasePositions = [
+      0, 0, 0,  //
+      1, 0, 0,  //
+      0, 1, 0,  //
+      1, 1, 0,  //
+      0, 0, 1,  //
+      1, 0, 1,  //
+      0, 1, 1,  //
+      1, 1, 1,  //
+    ];
+
+    // correct_index, vertex_position, uncorrected_index
+    // 0:  0, 0, 0   0
+    // 1:  1, 0, 0   1
+    // 2:  0, 1, 0   2
+    // 4:  0, 0, 1   3
+    // 5:  1, 0, 1   4
+    // 3:  1, 1, 0   5
+    // 6:  0, 1, 1   6
+    // 7:  1, 1, 1   7
+
+    // This maps uncorrected vertex indices to corrected vertex indices.
+    let vertexUncorrectedToCorrected = [0, 1, 2, 4, 5, 3, 6, 7];
+
+    // This maps corrected vertex indices to uncorrected vertex indices.
+    let vertexCorrectedToUncorrected = [0, 1, 2, 5, 3, 4, 6, 7];
+
+
+    // Page 666
+    let vertexBaseIndices = [
+      0, 1, 1, 4, 4, 7, 4, 7,  //
+      1, 5, 0, 1, 1, 4, 4, 7,  //
+      0, 2, 2, 5, 5, 7, 5, 7,  //
+      2, 6, 0, 2, 2, 5, 5, 7,  //
+      0, 3, 3, 6, 6, 7, 6, 7,  //
+      3, 4, 0, 3, 3, 6, 6, 7,  //
+    ];
+
+    // Determined by looking at the figure and determining the corresponding
+    // vertex order for each possible front vertex.
+    let vertexPermutation = [
+      0, 1, 2, 3, 4, 5, 6, 7,  //
+      1, 4, 5, 0, 3, 7, 2, 6,  //
+      2, 6, 0, 5, 7, 3, 1, 4,  //
+      3, 0, 6, 4, 1, 2, 7, 5,  //
+      4, 3, 7, 1, 0, 6, 5, 2,  //
+      5, 2, 1, 7, 6, 0, 4, 3,  //
+      6, 7, 3, 2, 5, 4, 0, 1,  //
+      7, 5, 4, 6, 2, 1, 3, 0,  //
+    ];
+
+    let vertexIndices: number[] = [];
+    for (var p = 0; p < 8; ++p) {
+      for (var i = 0; i < vertexBaseIndices.length; ++i) {
+        const vertexPermutationIndex = vertexCorrectedToUncorrected[p] * 8 + vertexBaseIndices[i];
+        vertexIndices.push(vertexUncorrectedToCorrected[vertexPermutation[vertexPermutationIndex]]);
+      }
+    }
+
+    this.vertexIndices = new Int32Array(vertexIndices);
+  }
+
+  static get(gl: GL) {
+    return gl.memoize.get('SliceViewShaderBuffers', () => new SliceViewShaderBuffers(gl));
+  }
+};
 
 class VolumeSliceVertexComputationManager extends RefCounted {
   data: SliceViewShaderBuffers;
@@ -277,31 +365,21 @@ for (int e = 0; e < 4; ++e) {
 };
 
 export class RenderLayer extends GenericSliceViewRenderLayer {
-  sources: VolumeChunkSource[][]|null = null;
+  sources: VolumeChunkSource[][];
   vertexComputationManager: VolumeSliceVertexComputationManager;
   constructor(
       multiscaleSource: MultiscaleVolumeChunkSource,
       {shaderError = makeWatchableShaderError(), sourceOptions = <VolumeSourceOptions> {}} = {}) {
-    super(multiscaleSource.chunkManager, multiscaleSource.getSources(sourceOptions)[0][0].spec, {
+    super(multiscaleSource.chunkManager, multiscaleSource.getSources(sourceOptions), {
       shaderError = makeWatchableShaderError(),
     } = {});
 
     let gl = this.gl;
     this.vertexComputationManager = VolumeSliceVertexComputationManager.get(gl);
 
-    let sources = this.sources = multiscaleSource.getSources(sourceOptions);
-    let sourceIds: number[][] = [];
-    for (let alternatives of sources) {
-      let alternativeIds: number[] = [];
-      sourceIds.push(alternativeIds);
-      for (let source of alternatives) {
-        alternativeIds.push(source.rpcId!);
-      }
-    }
-
     let sharedObject = this.registerDisposer(new SharedObject());
     sharedObject.RPC_TYPE_ID = VOLUME_RENDERLAYER_RPC_ID;
-    sharedObject.initializeCounterpart(this.chunkManager.rpc!, {'sources': sourceIds});
+    sharedObject.initializeCounterpart(this.chunkManager.rpc!, {'sources': this.sourceIds});
     this.rpcId = sharedObject.rpcId;
   }
 

src/neuroglancer/sliceview/renderlayer.ts
@@ -28,96 +28,6 @@ import {makeWatchableShaderError, WatchableShaderError} from 'neuroglancer/webgl
 import {getShaderType} from 'neuroglancer/webgl/shader_lib';
 import {RpcId, SharedObject} from 'neuroglancer/worker_rpc';
 
-
-export class SliceViewShaderBuffers extends RefCounted {
-  outputVertexIndices: Buffer;
-  vertexBasePositions: number[];
-  vertexIndices: Int32Array;
-  constructor(gl: GL) {
-    super();
-    this.outputVertexIndices = this.registerDisposer(
-        Buffer.fromData(gl, new Float32Array([0, 1, 2, 3, 4, 5]), gl.ARRAY_BUFFER, gl.STATIC_DRAW));
-
-    // This specifies the original, "uncorrected" vertex positions.
-    // var vertexBasePositions = [
-    //   0, 0, 0,
-    //   1, 0, 0,
-    //   0, 1, 0,
-    //   0, 0, 1,
-    //   1, 0, 1,
-    //   1, 1, 0,
-    //   0, 1, 1,
-    //   1, 1, 1,
-    // ];
-
-    // This specifies the "corrected" vertex positions.
-    this.vertexBasePositions = [
-      0, 0, 0,  //
-      1, 0, 0,  //
-      0, 1, 0,  //
-      1, 1, 0,  //
-      0, 0, 1,  //
-      1, 0, 1,  //
-      0, 1, 1,  //
-      1, 1, 1,  //
-    ];
-
-    // correct_index, vertex_position, uncorrected_index
-    // 0:  0, 0, 0   0
-    // 1:  1, 0, 0   1
-    // 2:  0, 1, 0   2
-    // 4:  0, 0, 1   3
-    // 5:  1, 0, 1   4
-    // 3:  1, 1, 0   5
-    // 6:  0, 1, 1   6
-    // 7:  1, 1, 1   7
-
-    // This maps uncorrected vertex indices to corrected vertex indices.
-    let vertexUncorrectedToCorrected = [0, 1, 2, 4, 5, 3, 6, 7];
-
-    // This maps corrected vertex indices to uncorrected vertex indices.
-    let vertexCorrectedToUncorrected = [0, 1, 2, 5, 3, 4, 6, 7];
-
-
-    // Page 666
-    let vertexBaseIndices = [
-      0, 1, 1, 4, 4, 7, 4, 7,  //
-      1, 5, 0, 1, 1, 4, 4, 7,  //
-      0, 2, 2, 5, 5, 7, 5, 7,  //
-      2, 6, 0, 2, 2, 5, 5, 7,  //
-      0, 3, 3, 6, 6, 7, 6, 7,  //
-      3, 4, 0, 3, 3, 6, 6, 7,  //
-    ];
-
-    // Determined by looking at the figure and determining the corresponding
-    // vertex order for each possible front vertex.
-    let vertexPermutation = [
-      0, 1, 2, 3, 4, 5, 6, 7,  //
-      1, 4, 5, 0, 3, 7, 2, 6,  //
-      2, 6, 0, 5, 7, 3, 1, 4,  //
-      3, 0, 6, 4, 1, 2, 7, 5,  //
-      4, 3, 7, 1, 0, 6, 5, 2,  //
-      5, 2, 1, 7, 6, 0, 4, 3,  //
-      6, 7, 3, 2, 5, 4, 0, 1,  //
-      7, 5, 4, 6, 2, 1, 3, 0,  //
-    ];
-
-    let vertexIndices: number[] = [];
-    for (var p = 0; p < 8; ++p) {
-      for (var i = 0; i < vertexBaseIndices.length; ++i) {
-        const vertexPermutationIndex = vertexCorrectedToUncorrected[p] * 8 + vertexBaseIndices[i];
-        vertexIndices.push(vertexUncorrectedToCorrected[vertexPermutation[vertexPermutationIndex]]);
-      }
-    }
-
-    this.vertexIndices = new Int32Array(vertexIndices);
-  }
-
-  static get(gl: GL) {
-    return gl.memoize.get('SliceViewShaderBuffers', () => new SliceViewShaderBuffers(gl));
-  }
-};
-
 const tempVec3 = vec3.create();
 const tempVec3b = vec3.create();
 const tempMat4 = mat4.create();
@@ -125,11 +35,12 @@ const tempMat4 = mat4.create();
 export abstract class RenderLayer extends GenericRenderLayer {
   chunkManager: ChunkManager;
   sources: SliceViewChunkSource[][]|null = null;
+  sourceIds: number[][] = [];
   shader: ShaderProgram|undefined = undefined;
   shaderUpdated = true;
   rpcId: RpcId|null = null;
   shaderError: WatchableShaderError;
-  constructor(chunkManager: ChunkManager, spec: SliceViewChunkSpecification, {
+  constructor(chunkManager: ChunkManager, sources: SliceViewChunkSource[][], {
     shaderError = makeWatchableShaderError(),
   } = {}) {
     super();
@@ -137,8 +48,20 @@ export abstract class RenderLayer extends GenericRenderLayer {
     this.shaderError = shaderError;
     shaderError.value = undefined;
     this.chunkManager = chunkManager;
+    console.log(sources);
+    this.sources = sources;
+    console.log(this.sources);
     let gl = this.gl;
 
+    for (let alternatives of sources) {
+      let alternativeIds: number[] = [];
+      this.sourceIds.push(alternativeIds);
+      for (let source of alternatives) {
+        alternativeIds.push(source.rpcId!);
+      }
+    }
+
+    let spec = this.sources[0][0].spec;
     let {chunkLayout} = spec;
 
     const voxelSize = this.voxelSize =
@@ -196,7 +119,7 @@ export abstract class RenderLayer extends GenericRenderLayer {
   getShaderKey() { return ''; }
 
   getShader() {
-    let key = this.getShaderKey(); 
+    let key = this.getShaderKey();
     return this.gl.memoize.get(key, () => this.buildShader());
   }
 
@@ -208,9 +131,9 @@ export abstract class RenderLayer extends GenericRenderLayer {
 
   abstract defineShader(builder: ShaderBuilder): void
 
-  abstract beginSlice(_sliceView: SliceView): ShaderProgram 
+  abstract beginSlice(_sliceView: SliceView): ShaderProgram
 
-  abstract endSlice(shader: ShaderProgram): void 
+  abstract endSlice(shader: ShaderProgram): void
 
-  abstract draw(sliceView: SliceView): void 
+  abstract draw(sliceView: SliceView): void
 }

src/neuroglancer/point/backend.ts
@@ -86,22 +86,7 @@ export abstract class PointChunkSource extends SliceViewChunkSource implements
 
 @registerSharedObject(POINT_RENDERLAYER_RPC_ID)
 export class RenderLayer extends SliceViewRenderLayer implements RenderLayerInterface {
-  rpcId: number;
   sources: PointChunkSource[][];
-
-  constructor(rpc: RPC, options: any) {
-    super(rpc, options);
-    let sources = this.sources = new Array<PointChunkSource[]>();
-    for (let alternativeIds of options['sources']) {
-      let alternatives = new Array<PointChunkSource>();
-      sources.push(alternatives);
-      for (let sourceId of alternativeIds) {
-        let source: PointChunkSource = rpc.get(sourceId);
-        this.registerDisposer(source.addRef());
-        alternatives.push(source);
-      }
-    }
-  }
 }
 
 

src/neuroglancer/point/frontend.ts
@@ -37,6 +37,7 @@ import {registerSharedObjectOwner, RPC, RpcId, SharedObject} from 'neuroglancer/
 const tempMat4 = mat4.create();
 
 export class RenderLayer extends GenericSliceViewRenderLayer {
+  sources: PointChunkSource[][];
   shader: ShaderProgram|undefined = undefined;
   shaderUpdated = true;
   rpcId: RpcId|null = null;
@@ -48,25 +49,15 @@ export class RenderLayer extends GenericSliceViewRenderLayer {
   constructor(
       multiscaleSource: MultiscalePointChunkSource,
       {shaderError = makeWatchableShaderError(), sourceOptions = <PointSourceOptions> {}} = {}) {
-    super(multiscaleSource.chunkManager, multiscaleSource.getSources(sourceOptions)[0][0].spec, {
+    super(multiscaleSource.chunkManager, multiscaleSource.getSources(sourceOptions), {
       shaderError = makeWatchableShaderError(),
     } = {});
 
     let gl = this.gl;
 
-    let sources = this.sources = multiscaleSource.getSources(sourceOptions);
-    let sourceIds: number[][] = [];
-    for (let alternatives of sources) {
-      let alternativeIds: number[] = [];
-      sourceIds.push(alternativeIds);
-      for (let source of alternatives) {
-        alternativeIds.push(source.rpcId!);
-      }
-    }
-
     let sharedObject = this.registerDisposer(new SharedObject());
     sharedObject.RPC_TYPE_ID = POINT_RENDERLAYER_RPC_ID;
-    sharedObject.initializeCounterpart(this.chunkManager.rpc!, {'sources': sourceIds});
+    sharedObject.initializeCounterpart(this.chunkManager.rpc!, {'sources': this.sourceIds});
     this.rpcId = sharedObject.rpcId;
   }
 

src/neuroglancer/sliceview/backend.ts
@@ -221,10 +221,24 @@ export abstract class SliceViewChunkSource extends ChunkSource implements
 }
 
 export abstract class RenderLayer extends SharedObjectCounterpart implements RenderLayerInterface {
+  rpcId: number;
   sources: SliceViewChunkSource[][];
   layerChanged = new NullarySignal();
 
-  // TODO: can we refactor this to simply let the volume and point renderlayer override the type of source? 
+  constructor(rpc: RPC, options: any) {
+    super(rpc, options);
+    let sources = this.sources = new Array<SliceViewChunkSource[]>();
+    for (let alternativeIds of options['sources']) {
+      let alternatives = new Array<SliceViewChunkSource>();
+      sources.push(alternatives);
+      for (let sourceId of alternativeIds) {
+        let source: SliceViewChunkSource = rpc.get(sourceId);
+        this.registerDisposer(source.addRef());
+        alternatives.push(source);
+      }
+    }
+  }
+
 }
 
 /**

src/neuroglancer/sliceview/volume/backend.ts
@@ -94,22 +94,7 @@ export abstract class VolumeChunkSource extends SliceViewChunkSource implements
 
 @registerSharedObject(VOLUME_RENDERLAYER_RPC_ID)
 export class RenderLayer extends SliceViewRenderLayer implements RenderLayerInterface {
-  rpcId: number;
   sources: VolumeChunkSource[][];
-
-  constructor(rpc: RPC, options: any) {
-    super(rpc, options);
-    let sources = this.sources = new Array<VolumeChunkSource[]>();
-    for (let alternativeIds of options['sources']) {
-      let alternatives = new Array<VolumeChunkSource>();
-      sources.push(alternatives);
-      for (let sourceId of alternativeIds) {
-        let source: VolumeChunkSource = rpc.get(sourceId);
-        this.registerDisposer(source.addRef());
-        alternatives.push(source);
-      }
-    }
-  }
 }
 
 /**

commit 153865fa19746cef81f25a5a9a33f689c0c80631

fix(sliceview): properly pass shaderError object
This fixes a regression introduced by 7ccd046c98fecfd650539426a9ec96caa34f5c75, which also prevented
building with the Closure Compiler.

src/neuroglancer/sliceview/volume/renderlayer.ts
@@ -372,9 +372,7 @@ export class RenderLayer extends GenericSliceViewRenderLayer {
   constructor(
       multiscaleSource: MultiscaleVolumeChunkSource,
       {shaderError = makeWatchableShaderError(), sourceOptions = <VolumeSourceOptions> {}} = {}) {
-    super(multiscaleSource.chunkManager, multiscaleSource.getSources(sourceOptions), {
-      shaderError = makeWatchableShaderError(),
-    } = {});
+    super(multiscaleSource.chunkManager, multiscaleSource.getSources(sourceOptions), {shaderError});
 
     let gl = this.gl;
     this.vertexComputationManager = VolumeSliceVertexComputationManager.get(gl);