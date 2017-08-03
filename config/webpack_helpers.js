cfe323f2dab42290525310885a5937d3846b27b3
Jeremy Maitin-Shepard <jbms@google.com>
2017-08-03 15:53:51 -0700

    chore: upgrade webpack dependency

--- config/webpack_helpers.js
+++ config/webpack_helpers.js
@@ -189,7 +189,7 @@ function getBaseConfig(options) {
         tsLoaderEntry, {test: /\.json$/, loader: require.resolve('json-loader')}, {
           test: /\.css$/,
           loader:
-              ExtractTextPlugin.extract({fallbackLoader: 'style-loader', loader: 'css-loader'})
+              ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})
         },
         {
           test: /\.glsl$/,
