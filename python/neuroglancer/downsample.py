bd8b1e19edb3c46da3cd902bd54dc4f24b67fc06
William Silversmith <william.silversmith@gmail.com>
2017-09-15 20:07:48 -0400

    feat: added downsample_with_mode capability for 2D downsampling
    Also added a requirements.txt, and a function downsample_segmentation
    that uses the new capability when possible but defaults to striding.

--- python/neuroglancer/downsample.py
+++ python/neuroglancer/downsample.py
@@ -16,6 +16,9 @@ from __future__ import division
 
 import math
 import numpy as np
+import operator
+
+from downsample_countless import downsample_with_mode_2D, odd_to_even
 
 
 def downsample_with_averaging(array, factor):
@@ -41,3 +44,57 @@ def downsample_with_striding(array, factor):
     @return: The downsampled array, of the same type as x.
     """
     return array[tuple(np.s_[::f] for f in factor)]
+
+
+def downsample_segmentation(data, factor):
+  """Downsample x by factor by picking the most frequent value within a
+  2x2 square for each 2D image within a 3D stack if factor
+  is specified such that a power of two downsampling is possible.
+
+  Otherwise, downsample by striding.
+
+  If factor has fewer parameters than data.shape, the remainder
+  are assumed to be 1.
+
+  @return: The downsampled array, of the same type as x.
+  """
+  factor = np.array(factor)
+  if np.all(np.array(factor, int) == 1):
+      return data
+
+  is_pot = lambda x: (x != 0) and not (x & (x - 1)) # is power of two
+  is_twod_pot_downsample = np.any(factor == 1) and is_pot(reduce(operator.mul, factor))
+  
+  if not is_twod_pot_downsample:
+    return downsample_with_striding(data, tuple(factor))
+
+  preserved_axis = np.where(factor == 1)[0][0] # e.g. 0, 1, 2
+
+  shape3d = np.array(data.shape[:3])
+
+  modulo_shape = shape3d % 2
+  modulo_shape[preserved_axis] = 0
+  has_even_dims = sum(modulo_shape) == 0 
+
+  # algorithm is written for xy plane, so
+  # switch other orientations to that plane, 
+  # do computation and switch back.
+  data = np.swapaxes(data, preserved_axis, 2)
+
+  if not has_even_dims:
+    data = odd_to_even(data)
+    shape3d = np.array(data.shape[:3])
+
+  output = np.zeros(
+    shape=( int(data.shape[0] / 2), int(data.shape[1] / 2), data.shape[2], data.shape[3]), 
+    dtype=data.dtype
+  )
+  for z in xrange(data.shape[2]):
+    output[:,:,z,:] = downsample_with_mode_2D(data[:,:,z,:])
+  
+  factor = factor / 2
+  factor[preserved_axis] = 1
+
+  output = np.swapaxes(output, preserved_axis, 2)
+  
+  return downsample_segmentation(output, factor)
