google/neuroglancer

commit c195285584f3fcc7bb1fa37595b803ca3d6d58b4

feat: interface for determining whether panels/layers are "ready" (i.e. all data loaded)

src/neuroglancer/mesh/backend.ts
@@ -35,7 +35,6 @@ export type FragmentId = string;
 
 // Chunk that contains the list of fragments that make up a single object.
 export class ManifestChunk extends Chunk {
-  backendOnly = true;
   objectId = new Uint64();
   fragmentIds: FragmentId[]|null;
   clipBounds?: Bounds;
@@ -55,6 +54,11 @@ export class ManifestChunk extends Chunk {
 
   freeSystemMemory() {
     this.fragmentIds = null;
+  }
+
+  serialize(msg: any, transfers: any[]) {
+    super.serialize(msg, transfers);
+    msg.fragmentIds = this.fragmentIds;
   }
 
   downloadSucceeded() {

src/neuroglancer/annotation/user_layer.ts
@@ -44,6 +44,7 @@ export class AnnotationPointListUserLayer extends UserLayer {
       let value = layerSelectedValues.get(this);
       this.selectedIndex.value = typeof value === 'number' ? value : null;
     }));
+    this.isReady = true;
   }
   toJSON() {
     let x: any = {'type': LAYER_TYPE};

src/neuroglancer/display_context.ts
@@ -34,6 +34,8 @@ export abstract class RenderedPanel extends RefCounted {
     this.context.scheduleRedraw();
   }
 
+  abstract isReady(): boolean;
+
   setGLViewport() {
     let element = this.element;
     const clientRect = element.getBoundingClientRect();
@@ -86,6 +88,18 @@ export class DisplayContext extends RefCounted {
     container.appendChild(canvas);
     this.gl = initializeWebGL(canvas);
     this.registerEventListener(window, 'resize', this.onResize.bind(this));
+  }
+
+  isReady() {
+    for (const panel of this.panels) {
+      if (!panel.visible) {
+        continue;
+      }
+      if (!panel.isReady()) {
+        return false;
+      }
+    }
+    return true;
   }
 
   /**

src/neuroglancer/image_user_layer.ts
@@ -23,7 +23,6 @@ import {VolumeType} from 'neuroglancer/sliceview/volume/base';
 import {FRAGMENT_MAIN_START, getTrackableFragmentMain, ImageRenderLayer} from 'neuroglancer/sliceview/volume/image_renderlayer';
 import {trackableAlphaValue} from 'neuroglancer/trackable_alpha';
 import {trackableBlendModeValue} from 'neuroglancer/trackable_blend';
-import {mat4} from 'neuroglancer/util/geom';
 import {makeWatchableShaderError} from 'neuroglancer/webgl/dynamic_shader';
 import {RangeWidget} from 'neuroglancer/widget/range';
 import {ShaderCodeWidget} from 'neuroglancer/widget/shader_code_widget';
@@ -65,6 +64,7 @@ export class ImageUserLayer extends UserLayer {
             });
             this.addRenderLayer(renderLayer);
             this.shaderError.changed.dispatch();
+            this.isReady = true;
           }
         });
   }

src/neuroglancer/layer.ts
@@ -87,6 +87,7 @@ export class UserLayer extends RefCounted {
   readyStateChanged = new NullarySignal();
   specificationChanged = new NullarySignal();
   renderLayers = new Array<RenderLayer>();
+  isReady = false;
   constructor(renderLayers: RenderLayer[] = []) {
     super();
     renderLayers.forEach(this.addRenderLayer.bind(this));
@@ -171,6 +172,11 @@ export class ManagedUserLayer extends RefCounted {
       this.readyStateChanged.dispatch();
       this.handleLayerChanged();
     }
+  }
+
+  isReady() {
+    const {layer} = this;
+    return layer !== null && layer.isReady;
   }
 
   /**

src/neuroglancer/mesh/frontend.ts
@@ -18,6 +18,7 @@ import {ChunkState} from 'neuroglancer/chunk_manager/base';
 import {Chunk, ChunkManager, ChunkSource} from 'neuroglancer/chunk_manager/frontend';
 import {FRAGMENT_SOURCE_RPC_ID, MESH_LAYER_RPC_ID} from 'neuroglancer/mesh/base';
 import {PerspectiveViewRenderContext, PerspectiveViewRenderLayer} from 'neuroglancer/perspective_view/render_layer';
+import {forEachVisibleSegment, getObjectKey} from 'neuroglancer/segmentation_display_state/base';
 import {forEachSegmentToDraw, getObjectColor, registerRedrawWhenSegmentationDisplayState3DChanged, SegmentationDisplayState3D, SegmentationLayerSharedObject} from 'neuroglancer/segmentation_display_state/frontend';
 import {mat4, vec3, vec4} from 'neuroglancer/util/geom';
 import {getObjectId} from 'neuroglancer/util/object_id';
@@ -175,6 +176,37 @@ export class MeshLayer extends PerspectiveViewRenderLayer {
 
     meshShaderManager.endLayer(gl, shader);
   }
+
+  isReady() {
+    const {displayState, source} = this;
+    let ready = true;
+    const fragmentChunks = source.fragmentSource.chunks;
+    forEachVisibleSegment(displayState, objectId => {
+      const key = getObjectKey(objectId, displayState.clipBounds.value);
+      const manifestChunk = source.chunks.get(key);
+      if (manifestChunk === undefined) {
+        ready = false;
+        return;
+      }
+      for (const fragmentId of manifestChunk.fragmentIds) {
+        const fragmentChunk = fragmentChunks.get(`${key}/${fragmentId}`);
+        if (fragmentChunk === undefined || fragmentChunk.state !== ChunkState.GPU_MEMORY) {
+          ready = false;
+          return;
+        }
+      }
+    });
+    return ready;
+  }
+}
+
+export class ManifestChunk extends Chunk {
+  fragmentIds: string[];
+
+  constructor(source: MeshSource, x: any) {
+    super(source);
+    this.fragmentIds = x.fragmentIds;
+  }
 }
 
 export class FragmentChunk extends Chunk {
@@ -214,10 +246,14 @@ export class FragmentChunk extends Chunk {
 
 export class MeshSource extends ChunkSource {
   fragmentSource = this.registerDisposer(new FragmentSource(this.chunkManager, this));
+  chunks: Map<string, ManifestChunk>;
   initializeCounterpart(rpc: RPC, options: any) {
     this.fragmentSource.initializeCounterpart(this.chunkManager.rpc!, {});
     options['fragmentSource'] = this.fragmentSource.addCounterpartRef();
     super.initializeCounterpart(rpc, options);
+  }
+  getChunk(x: any) {
+    return new ManifestChunk(this, x);
   }
 }
 

src/neuroglancer/perspective_view/panel.ts
@@ -182,6 +182,26 @@ export class PerspectivePanel extends RenderedDataPanel {
     return this.viewer.navigationState;
   }
 
+  isReady() {
+    if (!this.visible) {
+      return false;
+    }
+    if (this.viewer.showSliceViews.value) {
+      for (const sliceView of this.sliceViews) {
+        if (!sliceView.isReady()) {
+          return false;
+        }
+      }
+    }
+    let visibleLayers = this.visibleLayerTracker.getVisibleLayers();
+    for (let renderLayer of visibleLayers) {
+      if (!renderLayer.isReady()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   updateProjectionMatrix() {
     let projectionMat = this.projectionMat;
     mat4.perspective(projectionMat, Math.PI / 4.0, this.width / this.height, 10, 5000);

src/neuroglancer/perspective_view/render_layer.ts
@@ -64,4 +64,8 @@ export class PerspectiveViewRenderLayer extends VisibilityTrackedRenderLayer {
   get isTransparent() {
     return false;
   }
+
+  isReady() {
+    return true;
+  }
 }

commit c012b9a59fe51ef0f057af4ef4a384eca9d4ecce

fix(mesh): fix bug in mesh display introduced by commit c195285584f3fcc7bb1fa37595b803ca3d6d58b4

src/neuroglancer/mesh/backend.ts
@@ -324,7 +324,9 @@ export class MeshLayer extends SegmentationLayerSharedObjectCounterpart {
       let manifestChunk = source.getChunk(objectId, this.clipBounds.value);
       chunkManager.requestChunk(
           manifestChunk, priorityTier, basePriority + MESH_OBJECT_MANIFEST_CHUNK_PRIORITY);
-      if (manifestChunk.state === ChunkState.SYSTEM_MEMORY_WORKER) {
+      const state = manifestChunk.state;
+      if (state === ChunkState.SYSTEM_MEMORY_WORKER || state === ChunkState.SYSTEM_MEMORY ||
+          state === ChunkState.GPU_MEMORY) {
         for (let fragmentId of manifestChunk.fragmentIds!) {
           let fragmentChunk = source.getFragmentChunk(manifestChunk, fragmentId);
           chunkManager.requestChunk(