google/neuroglancer

commit cc6fdc944a5b759495d6f8a4f92369effdb0b09f

feat(python): rescale fixed-size viewer to fit window
When displaying a viewer with a fixed user-specified size (useful for generating screen shots of a
fixed size), rescale as needed for displaying in the browser so that the entire viewer is visible.
This makes it easier to preview what will be captured.

src/neuroglancer/display_context.ts
@@ -42,9 +42,11 @@ export abstract class RenderedPanel extends RefCounted {
     let element = this.element;
     const clientRect = element.getBoundingClientRect();
     const canvasRect = this.context.canvasRect!;
-    let left = element.clientLeft + clientRect.left - canvasRect.left;
+    const scaleX = element.clientWidth / clientRect.width;
+    const scaleY = element.clientHeight / clientRect.height;
+    let left = (element.clientLeft + clientRect.left - canvasRect.left) * scaleX;
     let width = element.clientWidth;
-    let top = clientRect.top - canvasRect.top + element.clientTop;
+    let top = (clientRect.top - canvasRect.top + element.clientTop) * scaleY;
     let height = element.clientHeight;
     let bottom = top + height;
     let gl = this.gl;

src/main_python.ts
@@ -148,14 +148,24 @@ window.addEventListener('DOMContentLoaded', () => {
       element.style.position = 'relative';
       element.style.width = null;
       element.style.height = null;
+      element.style.transform = null;
+      element.style.transformOrigin = null;
     } else {
       element.style.position = 'absolute';
       element.style.width = `${value[0]}px`;
       element.style.height = `${value[1]}px`;
+      const screenWidth = document.documentElement.clientWidth;
+      const screenHeight = document.documentElement.clientHeight;
+      const scaleX = screenWidth/value[0];
+      const scaleY = screenHeight/value[1];
+      const scale = Math.min(scaleX, scaleY);
+      element.style.transform = `scale(${scale})`;
+      element.style.transformOrigin = 'top left';
     }
     viewer.display.onResize();
   };
   updateSize();
+  window.addEventListener('resize', updateSize);
   size.changed.add(debounce(() => updateSize(), 0));
 
   const serverConnection = new ServerConnection(sharedState, privateState, configState);

commit 775a88347154be96b6af6aaee99655cf9caf2890

fix(display_context): fix incorrect offsets and scaling when rendering panels
This fixes a bug introduced by cc6fdc944a5b759495d6f8a4f92369effdb0b09f.

src/neuroglancer/display_context.ts
@@ -42,8 +42,8 @@ export abstract class RenderedPanel extends RefCounted {
     let element = this.element;
     const clientRect = element.getBoundingClientRect();
     const canvasRect = this.context.canvasRect!;
-    const scaleX = element.clientWidth / clientRect.width;
-    const scaleY = element.clientHeight / clientRect.height;
+    const scaleX = canvasRect.width / this.context.canvas.width;
+    const scaleY = canvasRect.height / this.context.canvas.height;
     let left = (element.clientLeft + clientRect.left - canvasRect.left) * scaleX;
     let width = element.clientWidth;
     let top = (clientRect.top - canvasRect.top + element.clientTop) * scaleY;