e138e53058af4b02602a9faebd1453dafc4146fa
Forrest Collman <forrest.collman@gmail.com>
2018-09-06 22:53:00 -0700

    adding json loading to python server

--- src/main_python.ts
+++ src/main_python.ts
@@ -119,7 +119,7 @@ window.addEventListener('DOMContentLoaded', () => {
   configState.add('screenshot', screenshotHandler.requestState);
 
   let sharedState: Trackable|undefined = viewer.state;
-
+  viewer.loadFromJsonUrl();
   if (window.location.hash) {
     const hashBinding = viewer.registerDisposer(new UrlHashBinding(viewer.state));
     hashBinding.updateFromUrlHash();
@@ -177,6 +177,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
   bindDefaultCopyHandler(viewer);
   bindDefaultPasteHandler(viewer);
-
-
 });
