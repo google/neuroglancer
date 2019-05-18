254480d9726e72555df8144e64bc001b7ae484b5
Jeremy Maitin-Shepard <jbms@google.com>
2019-05-17 17:57:38 -0700

    chore(python): bump package version to 1.1.0

--- python/setup.py
+++ python/setup.py
@@ -107,7 +107,7 @@ if platform.system() == 'Darwin':
 
 setup(
     name='neuroglancer',
-    version='1.0.15',
+    version='1.1.0',
     description='Python data backend for neuroglancer, a WebGL-based viewer for volumetric data',
     author='Jeremy Maitin-Shepard, Jan Funke',
     author_email='jbms@google.com, jfunke@iri.upc.edu',
