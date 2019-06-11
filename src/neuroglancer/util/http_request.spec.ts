3293e11f91e540cacdadf0b952a564eed20a0acb
Nico Kemnitz <nkemnitz@princeton.edu>
2019-06-10 22:59:54 -0400

    fix(http_request): Update tests

--- src/neuroglancer/util/http_request.spec.ts
+++ src/neuroglancer/util/http_request.spec.ts
@@ -1,7 +1,7 @@
 import {parseSpecialUrl} from 'neuroglancer/util/http_request';
 
 describe('Parsing Special URLs', () => {
-  let ezParseSpecialUrl = (url: string) => parseSpecialUrl(url)[0][0];
+  const ezParseSpecialUrl = (url: string) => parseSpecialUrl(url);
 
   it('Parse Normal URL', () => {
     expect(ezParseSpecialUrl('http://example.com/arg1/arg2/arg3/'))
@@ -13,16 +13,12 @@ describe('Parsing Special URLs', () => {
   });
 
   it('Parse Google Storage Url (gs://)', () => {
-    let result = parseSpecialUrl('gs://bucket/dataset/layer/');
-
-    expect(result[0][0]).toBe('https://storage.googleapis.com/bucket');
-    expect(result[1]).toBe('/dataset/layer/');
+    const result = parseSpecialUrl('gs://bucket/dataset/layer/');
+    expect(result).toBe('https://storage.googleapis.com/bucket/dataset/layer/');
   });
 
   it('Parse AWS S3 Url (s3://)', () => {
-    let result = parseSpecialUrl('s3://bucket/dataset/layer/');
-
-    expect(result[0][0]).toBe('https://s3.amazonaws.com/bucket');
-    expect(result[1]).toBe('/dataset/layer/');
+    const result = parseSpecialUrl('s3://bucket/dataset/layer/');
+    expect(result).toBe('https://s3.amazonaws.com/bucket/dataset/layer/');
   });
 });
