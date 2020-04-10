82c690241d09795a608dd62ad65e9a65c29a2b58
Oluwaseun Ogedengbe <seun40@gmail.com>
2020-04-10 15:10:14 -0400

    QOL changes to What's New also change functionality to support NUE of NG-EXTEND (#426)
    * changes whats new on first time visit
    
    * QOL changes to What's new
    
    * left variable in
    
    * removed unused file (these commits need squashing)

--- src/neuroglancer/whats_new/whats_new.ts
+++ src/neuroglancer/whats_new/whats_new.ts
@@ -1,7 +1,7 @@
+import 'neuroglancer/whats_new/whats_new.css';
+
 import {Overlay} from 'neuroglancer/overlay';
 import {Viewer} from 'neuroglancer/viewer';
-
-import 'neuroglancer/whats_new/whats_new.css';
 
 const updateFName = 'WHATS_NEW.md';
 const generateWhatsNew = (GHCommits: string[] = []) => {
@@ -24,15 +24,15 @@ const generateWhatsNew = (GHCommits: string[] = []) => {
 };
 
 export const findWhatsNew = async (viewer: Viewer) => {
-  let url =
-          `https://script.google.com/macros/s/AKfycbzVt6TLlJonmfU0EKTZVthi9pbM9dY1TYfTIH985tLUc8TZ5BNG/exec`,
-      WNCommits = JSON.parse(localStorage.getItem('WNCommits') || '[]'), headers = {
-        'Content-Type': 'text/plain;charset=utf-8',
-      },
-      body = JSON.stringify({
-        path: updateFName
-        // since: (WNCommits.length) ? WNCommits[0].commit.author.date : void(0)
-      });
+  // This google apps script is used by the seung-issues account to make an API request to Github.
+  // the api request queries Github to get a list of commits.
+  const url =
+      `https://script.google.com/macros/s/AKfycbzVt6TLlJonmfU0EKTZVthi9pbM9dY1TYfTIH985tLUc8TZ5BNG/exec`;
+  const WNCommits = JSON.parse(localStorage.getItem('WNCommits') || '[]');
+  const headers = {
+    'Content-Type': 'text/plain;charset=utf-8',
+  };
+  const body = JSON.stringify({path: updateFName});
 
   let GHRes = await fetch(url, {method: 'post', headers, body});
   let GHCommits = JSON.parse(await GHRes.json());
@@ -52,30 +52,31 @@ export const findWhatsNew = async (viewer: Viewer) => {
 };
 
 export class WhatsNewDialog extends Overlay {
-  constructor(public viewer: Viewer, description: string = '') {
+  constructor(public viewer: Viewer, description: string = '', override?: any) {
     super();
     let {content} = this;
 
-    if (!description.length) {
-      description = generateWhatsNew();
-    }
-
-    let modal = document.createElement('div');
-
+    const modal = document.createElement('div');
     content.appendChild(modal);
 
-    let header = document.createElement('h3');
-    header.textContent = `What's New`;
-    modal.appendChild(header);
-
+    if (!override) {
+      if (!description.length) {
+        description = generateWhatsNew();
+      }
+      let header = document.createElement('h3');
+      header.textContent = `What's New`;
+      modal.appendChild(header);
+    }
     let body = document.createElement('p');
     body.innerHTML = description;
     modal.appendChild(body);
 
     let okBtn = document.createElement('button');
+    okBtn.classList.add('neuroglancer-dialog-ok');
     okBtn.textContent = 'Ok';
     okBtn.onclick = () => this.dispose();
 
+    modal.classList.toggle('align-center', override ? override.center : false);
     modal.appendChild(okBtn);
     modal.onblur = () => this.dispose();
     modal.focus();
