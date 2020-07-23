07b7239e6b86a8b001d01be6fc279e564db8cd9c
Manuel Castro <macastro@princeton.edu>
2020-07-23 14:21:51 -0400

    Fix size of layer item (#479)

--- src/neuroglancer/layer_panel.ts
+++ src/neuroglancer/layer_panel.ts
@@ -168,6 +168,8 @@ class LayerWidget extends RefCounted {
   layerNumberElement: HTMLSpanElement;
   labelElement: HTMLSpanElement;
   valueElement: HTMLSpanElement;
+  maxLength: number = 0;
+  prevValueText: string = '';
 
   constructor(public layer: ManagedUserLayerWithSpecification, public panel: LayerPanel) {
     super();
@@ -438,6 +440,12 @@ export class LayerPanel extends RefCounted {
           });
           text += value.join(', ');
         }
+      }
+      if (text === widget.prevValueText) continue;
+      widget.prevValueText = text;
+      if (text.length > widget.maxLength) {
+        const length = widget.maxLength = text.length;
+        widget.valueElement.style.width = `${length}ch`;
       }
       widget.valueElement.textContent = text;
     }
