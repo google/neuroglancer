9ab7f1917adffe6cc046f76117f28705de5b2532
Jeremy Maitin-Shepard <jbms@google.com>
2020-10-01 11:01:07 -0700

    fix(empirical_cdf): fix CDF computation on Mac OS with Intel graphics
    Neuroglancer was leaving gl_PointSize unset; the behavior in that case is undefined by the WebGL
    spec; many drivers default to a size of 1, but some do not.

--- src/neuroglancer/webgl/empirical_cdf.ts
+++ src/neuroglancer/webgl/empirical_cdf.ts
@@ -102,6 +102,7 @@ if (stencilValue == 1.0) {
 } else {
   gl_Position = vec4(2.0 * dataValue * 255.0 / 256.0 - 1.0, 0.0, 0.0, 1.0);
 }
+gl_PointSize = 1.0;
 `);
     builder.setFragmentMain(`
 outputValue = vec4(1.0, 1.0, 1.0, 1.0);
