google/neuroglancer

commit 1fd1320ff8ab797b9cb4ca91ae468060fa279d75

feat(brainmaps): use greater parallelism to fetch multi-scale mesh fragments

src/neuroglancer/datasource/brainmaps/backend.ts
@@ -336,16 +336,18 @@ function combineBatchMeshFragments(fragments: BatchMeshResponseFragment[]) {
   return {vertexPositions: vertexBuffer, indices: indexBuffer};
 }
 
-function makeBatchMeshRequest(
+async function makeBatchMeshRequest<T>(
     credentialsProvider: CredentialsProvider<Credentials>,
     parameters: {instance: BrainmapsInstance, volumeId: string, meshName: string},
-    ids: Iterable<string>, cancellationToken: CancellationToken): Promise<ArrayBuffer> {
+    ids: Map<string, T>, cancellationToken: CancellationToken): Promise<ArrayBuffer> {
   const path = `/v1/objects/meshes:batch`;
-
   const batches: BatchMeshFragment[] = [];
   let prevObjectId: string|undefined;
   let batchSize = 0;
-  for (const id of ids) {
+  const pendingIds = new Map<string, T>();
+  for (const [id, idData] of ids) {
+    pendingIds.set(id, idData);
+    ids.delete(id);
     const splitIndex = id.indexOf('\0');
     const objectId = id.substring(0, splitIndex);
     const fragmentId = id.substring(splitIndex + 1);
@@ -360,14 +362,20 @@ function makeBatchMeshRequest(
     mesh_name: parameters.meshName,
     batches: batches,
   };
-  return makeRequest(
-      parameters['instance'], credentialsProvider, {
-        method: 'POST',
-        path,
-        payload: JSON.stringify(payload),
-        responseType: 'arraybuffer',
-      },
-      cancellationToken);
+  try {
+    return await makeRequest(
+        parameters['instance'], credentialsProvider, {
+          method: 'POST',
+          path,
+          payload: JSON.stringify(payload),
+          responseType: 'arraybuffer',
+        },
+        cancellationToken);
+  } finally {
+    for (const [id, idData] of pendingIds) {
+      ids.set(id, idData);
+    }
+  }
 }
 
 @registerSharedObject() export class BrainmapsMultiscaleMeshSource extends
@@ -396,7 +404,7 @@ function makeBatchMeshRequest(
         .then(response => decodeMultiscaleManifestChunk(chunk, response));
   }
 
-  downloadFragment(chunk: MultiscaleFragmentChunk, cancellationToken: CancellationToken) {
+  async downloadFragment(chunk: MultiscaleFragmentChunk, cancellationToken: CancellationToken) {
     const {parameters} = this;
 
     const manifestChunk = chunk.manifestChunk! as BrainmapsMultiscaleManifestChunk;
@@ -442,58 +450,69 @@ function makeBatchMeshRequest(
     idArray.sort((a, b) => defaultStringCompare(a[0], b[0]));
     ids = new Map(idArray);
 
-    function copyMeshData() {
-      fragments.sort((a, b) => a.chunkIndex - b.chunkIndex);
-      let indexOffset = 0;
-      const numSubChunks = 1 << (3 * (lod - prevLod));
-      const subChunkOffsets = new Uint32Array(numSubChunks + 1);
-      let prevSubChunkIndex = 0;
-      for (const fragment of fragments) {
-        const row = fragment.chunkIndex;
-        const subChunkIndex = getOctreeChildIndex(
-                                  octree[row * 5] >>> prevLod, octree[row * 5 + 1] >>> prevLod,
-                                  octree[row * 5 + 2] >>> prevLod) &
-            (numSubChunks - 1);
-        subChunkOffsets.fill(indexOffset, prevSubChunkIndex + 1, subChunkIndex + 1);
-        prevSubChunkIndex = subChunkIndex;
-        indexOffset += fragment.numIndices;
-      }
-      subChunkOffsets.fill(indexOffset, prevSubChunkIndex + 1, numSubChunks + 1);
-      assignMultiscaleMeshFragmentData(
-          chunk, {...combineBatchMeshFragments(fragments), subChunkOffsets},
-          VertexPositionFormat.float32);
-    }
-    function decodeResponse(response: ArrayBuffer): Promise<void>|void {
-      decodeBatchMeshResponse(
-          response, (fragment: BatchMeshResponseFragment&{chunkIndex: number}) => {
-            const chunkIndex = ids.get(fragment.fullKey)!;
-            if (!ids.delete(fragment.fullKey)) {
-              throw new Error(
-                  `Received unexpected fragment key: ${JSON.stringify(fragment.fullKey)}.`);
-            }
-            fragment.chunkIndex = chunkIndex;
-            fragments.push(fragment);
-          });
-
-      if (ids.size !== 0) {
-        // Partial response received.
-        return makeBatchRequest();
-      }
-      copyMeshData();
-    }
-
-    const {credentialsProvider} = this;
-
     const meshName = parameters.info.lods[lod].info.name;
 
-    function makeBatchRequest(): Promise<void> {
-      return makeBatchMeshRequest(
-                 credentialsProvider,
-                 {instance: parameters.instance, volumeId: parameters.volumeId, meshName},
-                 ids.keys(), cancellationToken)
-          .then(decodeResponse);
+    const parallelRequests = true;
+
+    await new Promise((resolve, reject) => {
+      let requestsInProgress = 0;
+      let error = false;
+      const maybeIssueMoreRequests = () => {
+        if (error) return;
+        while (ids.size !== 0) {
+          ++requestsInProgress;
+          makeBatchMeshRequest(
+              this.credentialsProvider,
+              {instance: parameters.instance, volumeId: parameters.volumeId, meshName}, ids,
+              cancellationToken)
+              .then(response => {
+                --requestsInProgress;
+                decodeBatchMeshResponse(
+                    response, (fragment: BatchMeshResponseFragment&{chunkIndex: number}) => {
+                      const chunkIndex = ids.get(fragment.fullKey)!;
+                      if (!ids.delete(fragment.fullKey)) {
+                        throw new Error(`Received unexpected fragment key: ${
+                            JSON.stringify(fragment.fullKey)}.`);
+                      }
+                      fragment.chunkIndex = chunkIndex;
+                      fragments.push(fragment);
+                    });
+                maybeIssueMoreRequests();
+              })
+            .catch(e => {
+              error = true;
+              reject(e);
+            });
+          if (!parallelRequests) break;
+        }
+        if (requestsInProgress === 0) {
+          resolve();
+          return;
+        }
+      };
+      maybeIssueMoreRequests();
+    });
+
+    // Combine fragments
+    fragments.sort((a, b) => a.chunkIndex - b.chunkIndex);
+    let indexOffset = 0;
+    const numSubChunks = 1 << (3 * (lod - prevLod));
+    const subChunkOffsets = new Uint32Array(numSubChunks + 1);
+    let prevSubChunkIndex = 0;
+    for (const fragment of fragments) {
+      const row = fragment.chunkIndex;
+      const subChunkIndex = getOctreeChildIndex(
+                                octree[row * 5] >>> prevLod, octree[row * 5 + 1] >>> prevLod,
+                                octree[row * 5 + 2] >>> prevLod) &
+          (numSubChunks - 1);
+      subChunkOffsets.fill(indexOffset, prevSubChunkIndex + 1, subChunkIndex + 1);
+      prevSubChunkIndex = subChunkIndex;
+      indexOffset += fragment.numIndices;
     }
-    return makeBatchRequest();
+    subChunkOffsets.fill(indexOffset, prevSubChunkIndex + 1, numSubChunks + 1);
+    assignMultiscaleMeshFragmentData(
+        chunk, {...combineBatchMeshFragments(fragments), subChunkOffsets},
+        VertexPositionFormat.float32);
   }
 }
 
@@ -547,39 +566,29 @@ function decodeManifestChunkWithSupervoxelIds(chunk: ManifestChunk, response: an
         .then(response => decodeManifestChunkWithSupervoxelIds(chunk, response));
   }
 
-  downloadFragment(chunk: FragmentChunk, cancellationToken: CancellationToken) {
+  async downloadFragment(chunk: FragmentChunk, cancellationToken: CancellationToken) {
     let {parameters} = this;
 
-    const ids = new Set<string>(JSON.parse(chunk.fragmentId!));
+    const ids = new Map<string, null>();
+    for (const id of JSON.parse(chunk.fragmentId!)) {
+      ids.set(id, null);
+    }
 
     let fragments: BatchMeshResponseFragment[] = [];
 
-    function copyMeshData() {
-      assignMeshFragmentData(chunk, combineBatchMeshFragments(fragments));
-    }
+    const {credentialsProvider} = this;
 
-    function decodeResponse(response: ArrayBuffer): Promise<void>|void {
+    while (ids.size !== 0) {
+      const response =
+          await makeBatchMeshRequest(credentialsProvider, parameters, ids, cancellationToken);
       decodeBatchMeshResponse(response, fragment => {
         if (!ids.delete(fragment.fullKey)) {
           throw new Error(`Received unexpected fragment key: ${JSON.stringify(fragment.fullKey)}.`);
         }
         fragments.push(fragment);
       });
-
-      if (ids.size !== 0) {
-        // Partial response received.
-        return makeBatchRequest();
-      }
-      copyMeshData();
     }
-
-    const {credentialsProvider} = this;
-
-    function makeBatchRequest(): Promise<void> {
-      return makeBatchMeshRequest(credentialsProvider, parameters, ids, cancellationToken)
-          .then(decodeResponse);
-    }
-    return makeBatchRequest();
+    assignMeshFragmentData(chunk, combineBatchMeshFragments(fragments));
   }
 }
 

commit 761968b5509c52a9a4d6154eef7d66613d58123f

fix(datasource/brainmaps): make chunk queue manager aware of parallel fragment requests
This fixes a problem introduced by 1fd1320ff8ab797b9cb4ca91ae468060fa279d75, which could lead to an
excessive number of concurrent downloads if a large number of objects is requested at once.

src/neuroglancer/chunk_manager/backend.ts
@@ -83,6 +83,7 @@ export class Chunk implements Disposable {
 
   private systemMemoryBytes_: number = 0;
   private gpuMemoryBytes_: number = 0;
+  private downloadSlots_: number = 1;
   backendOnly = false;
   isComputational = false;
   newlyRequestedToFrontend = false;
@@ -191,6 +192,20 @@ export class Chunk implements Disposable {
 
   get gpuMemoryBytes() {
     return this.gpuMemoryBytes_;
+  }
+
+  get downloadSlots() {
+    return this.downloadSlots_;
+  }
+
+  set downloadSlots(count: number) {
+    if (count === this.downloadSlots_) return;
+    updateChunkStatistics(this, -1);
+    this.chunkManager.queueManager.adjustCapacitiesForChunk(this, false);
+    this.downloadSlots_ = count;
+    this.chunkManager.queueManager.adjustCapacitiesForChunk(this, true);
+    updateChunkStatistics(this, 1);
+    this.chunkManager.queueManager.scheduleUpdate();
   }
 
   registerListener(listener: ChunkStateListener) {
@@ -700,7 +715,7 @@ export class ChunkQueueManager extends SharedObjectCounterpart {
       case ChunkState.DOWNLOADING:
         (chunk.isComputational ? this.computeCapacity :
                                  this.downloadCapacity[chunk.source!.sourceQueueLevel])
-            .adjust(factor, factor * chunk.systemMemoryBytes);
+            .adjust(factor * chunk.downloadSlots, factor * chunk.systemMemoryBytes);
         this.systemMemoryCapacity.adjust(factor, factor * chunk.systemMemoryBytes);
         break;
 

src/neuroglancer/datasource/brainmaps/backend.ts
@@ -485,6 +485,16 @@ async function makeBatchMeshRequest<T>(
             });
           if (!parallelRequests) break;
         }
+        // Notify the chunk queue of the number of download slots being used.  This partially limits
+        // parallelism by maximum number of concurrent downloads, and avoids fetch errors due to an
+        // excessive number of concurrent requests.
+        //
+        // Note that the limit on the number of concurrent downloads is not enforced perfectly.  If
+        // the new value of `downloadSlots` results in the total number of concurrent downloads
+        // exceeding the maximum allowed, the concurrent requests are still issued.  However, no
+        // additional lower-priority chunks will be promoted to `ChunkState.DOWNLOADING` until a
+        // download slot is available.
+        chunk.downloadSlots = Math.max(1, requestsInProgress);
         if (requestsInProgress === 0) {
           resolve();
           return;

templates/neuroglancer/util/linked_list.template.ts
@@ -70,4 +70,4 @@ export default class {
   static initializeHead<T extends Node<T>>(head: T) {
     head.NEXT_PROPERTY = head.PREV_PROPERTY = head;
   }
-};
+}