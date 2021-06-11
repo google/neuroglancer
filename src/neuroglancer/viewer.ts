813827e5cb35ea9e5739081f9b795e7df74a78a1
Jeremy Maitin-Shepard <jbms@google.com>
2021-06-10 17:38:48 -0700

    feat(viewer): add support for multiple credit links

--- src/neuroglancer/viewer.ts
+++ src/neuroglancer/viewer.ts
@@ -69,10 +69,12 @@ import {RPC} from 'neuroglancer/worker_rpc';
 
 declare var NEUROGLANCER_OVERRIDE_DEFAULT_VIEWER_OPTIONS: any
 
-declare var NEUROGLANCER_CREDIT_LINK: {
-  url: string,
-  text: string,
-}|undefined;
+interface CreditLink {
+  url: string;
+  text: string;
+}
+
+declare var NEUROGLANCER_CREDIT_LINK: CreditLink|CreditLink[]|undefined;
 
 export class DataManagementContext extends RefCounted {
   worker: Worker;
@@ -493,14 +495,20 @@ export class Viewer extends RefCounted implements ViewerState {
     topRow.appendChild(mousePositionWidget.element);
 
     if (typeof NEUROGLANCER_CREDIT_LINK !== 'undefined') {
-      const {url, text} = NEUROGLANCER_CREDIT_LINK!;
-      const creditLink = document.createElement('a');
-      creditLink.href = url;
-      creditLink.textContent = text;
-      creditLink.style.fontFamily = 'sans-serif';
-      creditLink.style.color = 'yellow';
-      creditLink.target = '_blank';
-      topRow.appendChild(creditLink);
+      let creditInfo = NEUROGLANCER_CREDIT_LINK!;
+      if (!Array.isArray(creditInfo)) {
+        creditInfo = [creditInfo];
+      }
+      for (const {url, text} of creditInfo) {
+        const creditLink = document.createElement('a');
+        creditLink.style.marginRight = '5px';
+        creditLink.href = url;
+        creditLink.textContent = text;
+        creditLink.style.fontFamily = 'sans-serif';
+        creditLink.style.color = 'yellow';
+        creditLink.target = '_blank';
+        topRow.appendChild(creditLink);
+      }
     }
 
     const annotationToolStatus =
