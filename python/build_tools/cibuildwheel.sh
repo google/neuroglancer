788d0171049f96851c893d776617dee051033cfb
Jeremy Maitin-Shepard <jbms@google.com>
2021-10-15 17:09:05 -0700

    chore(cibuildwheel): use manylinux2014 for pillow compatibility

--- python/build_tools/cibuildwheel.sh
+++ python/build_tools/cibuildwheel.sh
@@ -3,6 +3,8 @@
 export CIBW_SKIP="cp27-* cp36-* pp* *_i686 *-win32"
 export CIBW_TEST_EXTRAS="test"
 export CIBW_TEST_COMMAND="python -m pytest {project}/python/tests -vv -s --skip-browser-tests"
+export CIBW_MANYLINUX_X86_64_IMAGE=manylinux2014
+
 script_dir="$(dirname "$0")"
 root_dir="${script_dir}/../.."
 cd "${root_dir}"
