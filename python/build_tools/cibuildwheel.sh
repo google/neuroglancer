f9678aa13e0a0b46457205731dda47cd0604eb70
Jeremy Maitin-Shepard <jbms@google.com>
2021-10-15 17:35:16 -0700

    chore: exclude Python 3.10 until numpy packages are available for macOS

--- python/build_tools/cibuildwheel.sh
+++ python/build_tools/cibuildwheel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CIBW_SKIP="cp27-* cp36-* pp* *_i686 *-win32"
+export CIBW_SKIP="cp27-* cp36-* cp310-* pp* *_i686 *-win32"
 export CIBW_TEST_EXTRAS="test"
 export CIBW_TEST_COMMAND="python -m pytest {project}/python/tests -vv -s --skip-browser-tests"
 export CIBW_MANYLINUX_X86_64_IMAGE=manylinux2014
