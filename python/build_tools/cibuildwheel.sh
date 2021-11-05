73cb949f0b106a0af1e797d1bbc367a97875b3d5
Jeremy Maitin-Shepard <jbms@google.com>
2021-11-05 14:46:16 -0700

    chore: fix cibuildwheel testing issue on musllinux

--- python/build_tools/cibuildwheel.sh
+++ python/build_tools/cibuildwheel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-export CIBW_SKIP="cp27-* cp36-* cp310-* pp* *_i686 *-win32"
+export CIBW_SKIP="cp27-* cp36-* pp* *_i686 *-win32"
 export CIBW_TEST_EXTRAS="test"
 export CIBW_TEST_COMMAND="python -m pytest {project}/python/tests -vv -s --skip-browser-tests"
 export CIBW_MANYLINUX_X86_64_IMAGE=manylinux2014
+
+# Skip testing on CPython 3.10 and musllinux due to pillow binary wheels not being available.
+export CIBW_TEST_SKIP="cp310-* *-musllinux*"
 
 script_dir="$(dirname "$0")"
 root_dir="${script_dir}/../.."
