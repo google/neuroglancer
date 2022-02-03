ef9488129dac2e6480a91e1fb902c96f87d73a30
William Silversmith <william.silversmith@gmail.com>
2022-02-02 23:26:20 -0500

    docs: fix copyright year

--- src/neuroglancer/sliceview/png/png_wasm.c
+++ src/neuroglancer/sliceview/png/png_wasm.c
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2021 William Silvermsith
+ * Copyright 2022 William Silvermsith
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
