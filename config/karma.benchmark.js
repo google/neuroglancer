ee4b3d41046cd5afbd446c593298c1d6d9386b46
Jordan Matelsky <j6k4m8@gmail.com>
2022-03-14 15:07:16 -0400

    Remove typo from karma config

--- config/karma.benchmark.js
+++ config/karma.benchmark.js
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.dd
+ * limitations under the License.
  */
 
 'use strict';
@@ -42,4 +42,5 @@ module.exports = function(config) {
       require('./karma-esbuild'),
     ],
   });
-}
+};
+
