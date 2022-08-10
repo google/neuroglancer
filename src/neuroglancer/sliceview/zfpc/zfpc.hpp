dcc138e97bc83cf7e5a3018e7e7ef02b872773a1
William Silversmith <william.silversmith@gmail.com>
2022-08-10 17:01:32 -0400

    refactor: remove header writing code from zfpc cpp code

--- src/neuroglancer/sliceview/zfpc/zfpc.hpp
+++ src/neuroglancer/sliceview/zfpc/zfpc.hpp
@@ -31,39 +31,6 @@ Date: July 2022
 
 namespace zfpc {
 
-// little endian serialization of integers to chars
-// returns bytes written
-inline uint64_t itoc(uint8_t x, std::vector<unsigned char> &buf, uint64_t idx) {
-	buf[idx] = x;
-	return 1;
-}
-
-inline uint64_t itoc(uint16_t x, std::vector<unsigned char> &buf, uint64_t idx) {
-	buf[idx + 0] = x & 0xFF;
-	buf[idx + 1] = (x >> 8) & 0xFF;
-	return 2;
-}
-
-inline uint64_t itoc(uint32_t x, std::vector<unsigned char> &buf, uint64_t idx) {
-	buf[idx + 0] = x & 0xFF;
-	buf[idx + 1] = (x >> 8) & 0xFF;
-	buf[idx + 2] = (x >> 16) & 0xFF;
-	buf[idx + 3] = (x >> 24) & 0xFF;
-	return 4;
-}
-
-inline uint64_t itoc(uint64_t x, std::vector<unsigned char> &buf, uint64_t idx) {
-	buf[idx + 0] = x & 0xFF;
-	buf[idx + 1] = (x >> 8) & 0xFF;
-	buf[idx + 2] = (x >> 16) & 0xFF;
-	buf[idx + 3] = (x >> 24) & 0xFF;
-	buf[idx + 4] = (x >> 32) & 0xFF;
-	buf[idx + 5] = (x >> 40) & 0xFF;
-	buf[idx + 6] = (x >> 48) & 0xFF;
-	buf[idx + 7] = (x >> 56) & 0xFF;
-	return 8;
-}
-
 template <typename T>
 T ctoi(const unsigned char* buf, uint64_t idx = 0);
 
@@ -195,30 +162,6 @@ public:
 		}
 
 		return voxels() * data_width;
-	}
-
-	int64_t tochars(std::vector<unsigned char> &buf, uint64_t idx = 0) const {
-		if ((idx + ZfpcHeader::header_size) > buf.size()) {
-			return -1;
-		}
-
-		uint64_t i = idx;
-		for (int j = 0; j < 4; j++, i++) {
-			buf[i] = magic[j];
-		}
-
-		i += itoc(format_version, buf, i);
-		i += itoc(
-			static_cast<uint8_t>(data_type | ((mode << 3) & 0b111) | (c_order << 7)), 
-			buf, i
-		);
-		i += itoc(nx, buf, i);
-		i += itoc(ny, buf, i);
-		i += itoc(nz, buf, i);
-		i += itoc(nw, buf, i);
-		i += itoc(correlated_dims, buf, i);
-
-		return i - idx;
 	}
 
 	uint64_t num_streams() const {
