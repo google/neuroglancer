cbaa29dcb517bd33495d0ba44657035d2b36edfa
jan-matthis <mail@jan-matthis.de>
2022-08-18 11:02:57 +0000

    feat(python): support TensorStore data sources in LocalVolume

---
+++ python/tests/local_volume_test.py
@@ -0,0 +1,81 @@
+import neuroglancer
+import numpy as np
+try:
+    import tensorstore as ts
+except ImportError:
+    ts = None
+
+
+def test_numpy_data():
+    shape = (8, 16, 32)
+    data = np.zeros(shape)
+    local_volume = neuroglancer.LocalVolume(data)
+    assert local_volume.rank == len(shape)
+    assert local_volume.shape == shape
+    dimensions = neuroglancer.CoordinateSpace(
+        names=local_volume.data._default_labels,
+        units=local_volume.data._default_units,
+        scales=local_volume.data._default_scales)
+    assert local_volume.dimensions.to_json() == dimensions.to_json()
+
+
+def test_tensorstore_defaults():
+    if ts is None:
+        return
+    shape = (8, 16, 32)
+    data = ts.open({
+        'driver': 'n5',
+        'kvstore': {
+            'driver': 'memory',
+        },
+        'metadata': {
+            'dataType': 'uint8',
+            'dimensions': shape,
+        },
+        'create': True,
+        'delete_existing': True,
+      }).result()
+    local_volume = neuroglancer.LocalVolume(data)
+    assert local_volume.rank == len(shape)
+    assert local_volume.shape == shape
+    dimensions = neuroglancer.CoordinateSpace(
+        names=local_volume.data._default_labels,
+        units=local_volume.data._default_units,
+        scales=local_volume.data._default_scales)
+    assert local_volume.dimensions.to_json() == dimensions.to_json()
+
+
+def test_tensorstore_features():
+    if ts is None:
+        return
+    shape = (8, 16, 32)
+    offset = (2, 4, 8)
+    labels = ['x', 'y', '']
+    units = ['m', 'm', 'm']
+    scales = [1., 1., 0.5]
+    data = ts.open({
+        'driver': 'n5',
+        'kvstore': {
+            'driver': 'memory',
+        },
+        'metadata': {
+            'dataType': 'uint8',
+            'dimensions': shape,
+            'units': units,
+            'resolution': scales,
+        },
+        'transform': {
+            'input_labels': labels,
+            'input_inclusive_min': offset,
+        },
+        'create': True,
+        'delete_existing': True,
+      }).result()
+    local_volume = neuroglancer.LocalVolume(data)
+    assert local_volume.rank == len(shape)
+    assert local_volume.shape == tuple([s-o for s, o in zip(shape, offset)])
+    dimensions = neuroglancer.CoordinateSpace(
+        names=['x', 'y', 'd2'],
+        units=units,
+        scales=scales)
+    assert local_volume.dimensions.to_json() == dimensions.to_json()
