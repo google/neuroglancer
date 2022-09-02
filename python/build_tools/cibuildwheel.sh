d581d4de4dc7c228d1bf0500e1a9d177b5a82dfe
Jeremy Maitin-Shepard <jbms@google.com>
2022-09-02 12:04:02 -0700

    chore(github): enable testing on Python 3.10 and Python 3.11

--- python/build_tools/cibuildwheel.sh
+++ python/build_tools/cibuildwheel.sh
@@ -6,8 +6,8 @@ export CIBW_TEST_EXTRAS="test"
 export CIBW_TEST_COMMAND="python -m pytest {project}/python/tests -vv -s --skip-browser-tests"
 export CIBW_MANYLINUX_X86_64_IMAGE=manylinux2014
 
-# Skip testing on CPython 3.10, 3.11 and musllinux due to pillow binary wheels not being available.
-export CIBW_TEST_SKIP="cp310-* cp311-* *-musllinux*"
+# Skip testing on musllinux due to pillow binary wheels not being available.
+export CIBW_TEST_SKIP="*-musllinux*"
 
 script_dir="$(dirname "$0")"
 root_dir="${script_dir}/../.."
