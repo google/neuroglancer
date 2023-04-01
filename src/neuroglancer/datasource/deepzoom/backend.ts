706b6363a929eb224df84781c459b32b399fd631
Gergely Csucs <Tevemadar@users.noreply.github.com>
2023-04-01 18:34:00 +0200

    Compatibility variant
    - PNG and JPEG only
    - but works on older Firefox and Safari.

--- src/neuroglancer/datasource/deepzoom/backend.ts
+++ src/neuroglancer/datasource/deepzoom/backend.ts
@@ -17,25 +17,24 @@
 import {WithParameters} from 'neuroglancer/chunk_manager/backend';
 import {WithSharedCredentialsProviderCounterpart} from 'neuroglancer/credentials_provider/shared_counterpart';
 import {ImageTileEncoding, ImageTileSourceParameters} from 'neuroglancer/datasource/deepzoom/base';
-import {ChunkDecoder} from 'neuroglancer/sliceview/backend_chunk_decoders';
-import {decodeJpegChunk} from 'neuroglancer/sliceview/backend_chunk_decoders/jpeg';
-import {decodePngChunk} from 'neuroglancer/sliceview/backend_chunk_decoders/png';
 import {VolumeChunk, VolumeChunkSource} from 'neuroglancer/sliceview/volume/backend';
 import {CancellationToken} from 'neuroglancer/util/cancellation';
-// import {isNotFoundError, responseArrayBuffer} from 'neuroglancer/util/http_request';
+import {isNotFoundError, responseArrayBuffer} from 'neuroglancer/util/http_request';
 import {cancellableFetchSpecialOk, SpecialProtocolCredentials} from 'neuroglancer/util/special_protocol_request';
 import {registerSharedObject} from 'neuroglancer/worker_rpc';
+import { decodeJpeg } from 'src/neuroglancer/async_computation/decode_jpeg_request';
+import { decodePng } from 'src/neuroglancer/async_computation/decode_png_request';
+import { requestAsyncComputation } from 'src/neuroglancer/async_computation/request';
+import { transposeArray2d } from 'src/neuroglancer/util/array';
 
-const chunkDecoders = new Map<ImageTileEncoding, ChunkDecoder>();
-chunkDecoders.set(ImageTileEncoding.JPG, decodeJpegChunk);
-chunkDecoders.set(ImageTileEncoding.JPEG, decodeJpegChunk);
-chunkDecoders.set(ImageTileEncoding.PNG, decodePngChunk);
-
-declare var OffscreenCanvas: any; // shutting up some outdated compiler(?)
+/* This is enough if support for these aren't needed:
+ * - Firefox before 105 (OffscreenCanvas, 2022-09-20)
+ * - Safari before 16.4 (OffscreenCanvas, 2023-03-27)
+ */
+// declare var OffscreenCanvas: any; // shutting up some outdated compiler(?)
 
 @registerSharedObject() export class DeepzoomImageTileSource extends
 (WithParameters(WithSharedCredentialsProviderCounterpart<SpecialProtocolCredentials>()(VolumeChunkSource), ImageTileSourceParameters)) {
-  chunkDecoder = chunkDecoders.get(this.parameters.encoding)!;
   gridShape = (() => {
     const gridShape = new Uint32Array(3);
     const {upperVoxelBound, chunkDataSize} = this.spec;
@@ -48,52 +47,84 @@ declare var OffscreenCanvas: any; // shutting up some outdated compiler(?)
   async download(chunk: VolumeChunk, cancellationToken: CancellationToken): Promise<void> {
     const {parameters} = this;
 
-    /* This block is enough if support for these aren't needed:
-     * - Firefox before 105 (OffscreenCanvas, 2022-09-20)
-     * - Safari before 16.4 (OffscreenCanvas, 2023-03-27)
-     */
-    const {tilesize, overlap} = parameters;
-    const [x, y] = chunk.chunkGridPosition;
-    const url = `${parameters.url}/${x}_${y}.${ImageTileEncoding[parameters.encoding].toLowerCase()}`;
-    const response: Blob = await cancellableFetchSpecialOk(this.credentialsProvider, url, {}, response => response.blob(), cancellationToken);
-    const tile = await createImageBitmap(response);
-    const canvas = new OffscreenCanvas(tilesize, tilesize);
-    const ctx = canvas.getContext("2d")!;
-    ctx.drawImage(tile, x === 0 ? 0 : -overlap, y === 0 ? 0 : -overlap);
-    const id = ctx.getImageData(0, 0, tilesize, tilesize).data;
-    const t2 = tilesize * tilesize;
-    const d = chunk.data = new Uint8Array(t2 * 3);
-    for (let i = 0; i < t2; i++) {
-      d[i] = id[i * 4];
-      d[i + t2] = id[i * 4 + 1];
-      d[i + 2 * t2] = id[i * 4 + 2];
-    }
-
-    // let response: ArrayBuffer|undefined;
-    //   let url: string;
-    //   {
-    //     // chunkPosition must not be captured, since it will be invalidated by the next call to
-    //     // computeChunkBounds.
-    //     // let chunkPosition = this.computeChunkBounds(chunk);
-    //     this.computeChunkBounds(chunk);
-    //     // let chunkDataSize = chunk.chunkDataSize!;
-    //     // url = `${parameters.url}/${chunkPosition[0]}-${chunkPosition[0] + chunkDataSize[0]}_` +
-    //     //     `${chunkPosition[1]}-${chunkPosition[1] + chunkDataSize[1]}_` +
-    //     //     `${chunkPosition[2]}-${chunkPosition[2] + chunkDataSize[2]}`;
-    //     url = `${parameters.url}/${chunk.chunkGridPosition[0]}_${chunk.chunkGridPosition[1]}.${ImageTileEncoding[parameters.encoding].toLowerCase()}`;
-    //   }
-    //   try {
-    //     response = await cancellableFetchSpecialOk(
-    //         this.credentialsProvider, url, {}, responseArrayBuffer, cancellationToken);
-    //   } catch (e) {
-    //     if (isNotFoundError(e)) {
-    //       response = undefined;
-    //     } else {
-    //       throw e;
-    //     }
-    //   }
-    // if (response !== undefined) {
-    //   await this.chunkDecoder(chunk, cancellationToken, response);
+    // /* This block is enough if support for these aren't needed:
+    //  * - Firefox before 105 (OffscreenCanvas, 2022-09-20)
+    //  * - Safari before 16.4 (OffscreenCanvas, 2023-03-27)
+    //  */
+    // const {tilesize, overlap} = parameters;
+    // const [x, y] = chunk.chunkGridPosition;
+    // const url = `${parameters.url}/${x}_${y}.${ImageTileEncoding[parameters.encoding].toLowerCase()}`;
+    // const response: Blob = await cancellableFetchSpecialOk(this.credentialsProvider, url, {}, response => response.blob(), cancellationToken);
+    // const tile = await createImageBitmap(response);
+    // const canvas = new OffscreenCanvas(tilesize, tilesize);
+    // const ctx = canvas.getContext("2d")!;
+    // ctx.drawImage(tile, x === 0 ? 0 : -overlap, y === 0 ? 0 : -overlap);
+    // const id = ctx.getImageData(0, 0, tilesize, tilesize).data;
+    // const t2 = tilesize * tilesize;
+    // const d = chunk.data = new Uint8Array(t2 * 3);
+    // for (let i = 0; i < t2; i++) {
+    //   d[i] = id[i * 4];
+    //   d[i + t2] = id[i * 4 + 1];
+    //   d[i + 2 * t2] = id[i * 4 + 2];
     // }
+    // Todo: ^ "transposeArray2d" likely does the same
+
+    const {tilesize, overlap, encoding}=parameters;
+    const [x, y] = chunk.chunkGridPosition;
+    const ox = x === 0 ? 0 : overlap;
+    const oy = y === 0 ? 0 : overlap;
+    const url = `${parameters.url}/${x}_${y}.${ImageTileEncoding[encoding].toLowerCase()}`;
+    try{
+      const responseBuffer = await cancellableFetchSpecialOk(this.credentialsProvider, url, {}, responseArrayBuffer, cancellationToken);
+
+      const responseDataView = new DataView(responseBuffer);
+      let tilewidth=0,tileheight=0;
+      let tiledata: Uint8Array|undefined;
+      switch(encoding){
+        case ImageTileEncoding.PNG:
+          /* Somewhat straightforward, width and height are at fixed position
+           * no validation attempt, decoding throws on mismatch/garbage anyway
+           */
+          tilewidth = responseDataView.getInt32(16,false);
+          tileheight = responseDataView.getInt32(20,false);
+          tiledata = await requestAsyncComputation(
+            decodePng, cancellationToken, [responseBuffer],
+            new Uint8Array(responseBuffer),tilewidth,tileheight,3,1,false
+          );
+          tiledata=transposeArray2d(tiledata,tilewidth*tileheight,3);
+          break;
+
+        case ImageTileEncoding.JPG:
+        case ImageTileEncoding.JPEG:
+          /* Fragile heuristic with 10-minute read into the standard, deliberately ignoring some details */
+          let pos = 2; // skip SOI, it has irregular format anyway
+          while(pos < responseDataView.byteLength) {
+            if(responseDataView.getUint8(pos)!==0xFF)
+              throw new Error("The JPEG thing has broketh.");
+            const code = responseDataView.getUint8(pos+1);
+            if(code<0xC0 || code>0xCF)
+              pos+=responseDataView.getUint16(pos+2,false)+2;
+            else {
+              tileheight=responseDataView.getUint16(pos+5);
+              tilewidth=responseDataView.getUint16(pos+7);
+              tiledata=await requestAsyncComputation(
+                decodeJpeg, cancellationToken, [responseBuffer],
+                new Uint8Array(responseBuffer), tilewidth, tileheight, 3, false);
+              break;
+            }
+          }
+          break;
+      }
+      if(tiledata!==undefined){
+        const d = chunk.data = new Uint8Array(tilesize*tilesize*3);
+        for(let k=0;k<3;k++)
+          for(let j=0;j<tileheight;j++)
+            for(let i=0;i<tilewidth;i++)
+              d[i+j*tilesize+k*tilesize*tilesize]=tiledata[i+ox+(j+oy)*tilewidth+k*tilewidth*tileheight];
+      }
+    } catch (e) {
+      if (!isNotFoundError(e)) 
+        throw e;
+    }
   }
 }
--- src/neuroglancer/datasource/deepzoom/log.md
+++ src/neuroglancer/datasource/deepzoom/log.md
@@ -41,3 +41,10 @@ Supposedly this is the last point where `deepzoom` data source still opens `prec
 DZI descriptor is parsed, and used to fake the array of `ScaleInfo` structure. A simple backend hack is employed, skipping all the magic with partial chunks, but using [`createImageBitmap()`](https://developer.mozilla.org/en-US/docs/Web/API/createImageBitmap) (with a `Blob` in particular) for decoding the tile and [`OffscreenCanvas`](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas) for making it fixed size and also trimming the overlay (Deep Zoom tiles are allowed to have overlay with their neighbors for dubious reasons).  
 `OffscreenCanvas` needs relatively recent Firefox (105, 2022-09-20), and brand new Safari (16.4, 2023-03-27 - which was 4 days ago at the time of writing).  
 `raw` encoding is removed now. While `createImageBitmap()` could handle whatever image format the browser supports, the filtering is kept in place and `png`, `jpeg`, and `jpg` formats are accepted only.
+
+4. Compatibility version
+
+This one works on older Firefox and Safari versions.  
+
+The built-in `png` and `jpeg` decoders are brought into action. As there are known Deep Zoom tile generators putting an extra `overlay` pixel row on right/bottom edge tiles, the specify-image-dimensions-beforehand requirement is solved with actually reading into the files (`PNG` has its dimensions at fixed positions, and in case of `JPEG` the first occurrence of a `SIFn` chunk is assumed to tell the full dimensions).  
+Extra quirk is that the `JPEG` decoder produces planar image (there is an explicit `transposeArray2d()` call for that in `decode_jpeg.ts`), while `PNG` returns packed (like `getImageData()` in the previous variant).
