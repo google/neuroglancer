084d14c8be20f06610629ee13be0b9a91bacccef
Huy Nguyen <me@huy.dev>
2023-04-27 15:01:53 -0400

    Add new "2D" layout that only contains XY slice view

--- src/neuroglancer/data_panel_layout.ts
+++ src/neuroglancer/data_panel_layout.ts
@@ -89,6 +89,7 @@ const oneSquareSymbol = '◻';
 const LAYOUT_SYMBOLS = new Map<string, string>([
   ['4panel', '◱'],
   ['3d', oneSquareSymbol],
+  ['2d', '2D'],
 ]);
 
 export function makeSliceView(viewerState: SliceViewViewerState, baseToSelf?: quat) {
@@ -433,6 +434,12 @@ export const LAYOUTS = new Map<string, {
         '3d', {
           factory: (container, element, viewer, crossSections) =>
               new SinglePerspectiveLayout(container, element, viewer, crossSections)
+        }
+      ],
+      [
+        '2d', {
+          factory: (container, element, viewer) =>
+            new SinglePanelLayout(container, element, viewer, "xy")
         }
       ],
     ],
