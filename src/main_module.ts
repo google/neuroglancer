da9da87ae659691b2a9c128b58e3cc426ff643f4
Huy Nguyen <me@huy.dev>
2023-04-27 15:07:26 -0400

    Clean up main_modulee

--- src/main_module.ts
+++ src/main_module.ts
@@ -1,70 +1,38 @@
 import {bindDefaultCopyHandler, bindDefaultPasteHandler} from 'neuroglancer/ui/default_clipboard_handling';
 import {setDefaultInputEventBindings} from 'neuroglancer/ui/default_input_event_bindings';
 import {makeMinimalViewer} from 'neuroglancer/ui/minimal_viewer';
-// import {UrlHashBinding} from 'neuroglancer/ui/url_hash_binding';
 import { registerPositionWidgetTool } from './neuroglancer/widget/registerPositionWidgetTools';
+export {makeLayer} from "neuroglancer/layer"
+import {disableContextMenu, disableWheel} from 'neuroglancer/ui/disable_default_actions';
+export {DEFAULT_FRAGMENT_MAIN} from "neuroglancer/sliceview/volume/image_renderlayer"
 
-import 'neuroglancer/datasource/dvid/register_credentials_provider';
-import 'neuroglancer/datasource/dvid/register_default';
-import 'neuroglancer/datasource/brainmaps/register_default';
-import 'neuroglancer/datasource/precomputed/register_default';
-import 'neuroglancer/segmentation_user_layer';
-import 'neuroglancer/image_user_layer';
-
-import {defaultCredentialsManager} from 'neuroglancer/credentials_provider/default_manager';
-import {credentialsKey} from 'neuroglancer/datasource/brainmaps/api';
-import {BrainmapsCredentialsProvider} from 'neuroglancer/datasource/brainmaps/credentials_provider';
-
-
-/**
- * Sets up the default neuroglancer viewer.
- */
-// TODO: options here could enable or disable datasources.
-
-// TODO: need to check for webGL2 support
-// const gl = document.createElement('canvas').getContext('webgl2');
-// if (!gl) {
-//   console.log('your browser/OS/drivers do not support WebGL2');
-// } else {
-//   console.log('webgl2 works!');
-// }
-
-export function setupDefaultViewer(options: {
-  brainMapsClientId: string | undefined,
-  target: HTMLElement | undefined,
-}) {
-  // register_brainmaps
-  if (options.brainMapsClientId) {
-    const clientId: string = options.brainMapsClientId;
-    defaultCredentialsManager.register(credentialsKey, () => new BrainmapsCredentialsProvider(clientId));
-  }
-
-  let viewer = makeMinimalViewer({ bundleRoot: options.bundleRoot }, options.target);
-  setDefaultInputEventBindings(viewer.inputEventBindings);
-  bundleRoot: string | undefined,
-  chunkWorkerFileName: string,
-
-  /* const hashBinding = viewer.registerDisposer(new UrlHashBinding(viewer.state));
-  viewer.registerDisposer(hashBinding.parseError.changed.add(() => {
-    const {value} = hashBinding.parseError;
-    if (value !== undefined) {
-      const status = new StatusMessage();
-      status.setErrorMessage(`Error parsing state: ${value.message}`);
-      console.log('Error parsing state', value);
-    }
-    hashBinding.parseError;
-  }));
-  hashBinding.updateFromUrlHash(); */
-  registerPositionWidgetTool()
-
-  bindDefaultCopyHandler(viewer);
-  bindDefaultPasteHandler(viewer);
-
-  return viewer;
-}
+import "neuroglancer/datasource/precomputed/register_default"
+import "neuroglancer/datasource/zarr/register_default"
+import "neuroglancer/image_user_layer";
+import "neuroglancer/annotation/user_layer"
 
 export default class Neuroglancer {
   version() {
     return '0.0.1';
   }
+}
+
+
+export const hedwigSetup = (options: {
+  target: HTMLElement | undefined,
+  bundleRoot: string | undefined,
+  chunkWorkerFileName: string,
+}) => {
+
+  registerPositionWidgetTool()
+
+  disableContextMenu();
+  disableWheel();
+  let viewer = makeMinimalViewer({
+    chunkWorkerFileName: options.chunkWorkerFileName,
+  }, options.target);
+  setDefaultInputEventBindings(viewer.inputEventBindings);
+  bindDefaultCopyHandler(viewer);
+  bindDefaultPasteHandler(viewer);
+  return viewer;
 }
