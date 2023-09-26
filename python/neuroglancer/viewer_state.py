3da8bd348e740f731d08dfd682a5498967daee1c
Sean Martin <martins7@tcd.ie>
2023-09-26 11:58:15 +0100

    feat(python): Bind to volume rendering checkbox Wrapped property and tool

--- python/neuroglancer/viewer_state.py
+++ python/neuroglancer/viewer_state.py
@@ -157,6 +157,11 @@ class OpacityTool(Tool):
     __slots__ = ()
     TOOL_TYPE = 'opacity'
 
+@export_tool
+class VolumeRenderingTool(Tool):
+    __slots__ = ()
+    TOOL_TYPE = 'volumeRendering'
+
 
 @export_tool
 class CrossSectionRenderScaleTool(Tool):
@@ -499,6 +504,7 @@ class ImageLayer(Layer, _AnnotationLayerOptions):
     shader_controls = shaderControls = wrapped_property('shaderControls', ShaderControls)
     opacity = wrapped_property('opacity', optional(float, 0.5))
     blend = wrapped_property('blend', optional(str))
+    volume_rendering = wrapped_property('volumeRendering', optional(bool, False))
     cross_section_render_scale = crossSectionRenderScale = wrapped_property(
         'crossSectionRenderScale', optional(float, 1))
 
