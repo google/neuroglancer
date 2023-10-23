90c5c89146542eee424f334a46f936de574cbe06
Sean Martin <martins7@tcd.ie>
2023-10-23 12:18:35 +0100

    temp: texture fetch scaffolding

--- src/neuroglancer/widget/transfer_function.ts
+++ src/neuroglancer/widget/transfer_function.ts
@@ -27,21 +27,26 @@ import {ShaderBuilder, ShaderCodePart} from 'neuroglancer/webgl/shader';
 import {LayerControlFactory, LayerControlTool} from 'neuroglancer/widget/layer_control';
 import {Tab} from 'neuroglancer/widget/tab_view';
 import {UserLayer} from 'src/neuroglancer/layer';
+import {getSquareCornersBuffer} from 'src/neuroglancer/webgl/square_corners_buffer';
+import {setRawTextureParameters} from 'src/neuroglancer/webgl/texture';
 
 // const NUM_TF_LINES = 256;
 const TOOL_INPUT_EVENT_MAP = EventActionMap.fromObject({
   'at:shift?+mousedown0': {action: 'add-point'},
 });
+const transferFunctionSamplerTextureUnit = Symbol('histogramSamplerTexture');
 
 export class TransferFunctionPanel extends IndirectRenderedPanel {
   get drawOrder() {
-    return 0;
+    return 1;
   }
   constructor(public parent: TransferFunctionWidget) {
     super(parent.display, document.createElement('div'), parent.visibility);
     const {element} = this;
     element.classList.add('neuroglancer-transfer-function-panel');
   }
+
+  private cornersBuffer = getSquareCornersBuffer(this.gl);
 
   private lineShader = this.registerDisposer((() => {
     const builder = new ShaderBuilder(this.gl);
@@ -58,9 +63,30 @@ out_color = vec4(0.0, 1.0, 1.0, getLineAlpha());
     return builder.build();
   })());
 
+  private transferFunctionShader = this.registerDisposer((() => {
+    const builder = new ShaderBuilder(this.gl);
+    builder.addAttribute('vec2', 'aVertexPosition');
+    builder.addOutputBuffer('vec4', 'out_color', 0);
+    builder.addTextureSampler('sampler2D', 'uSampler', transferFunctionSamplerTextureUnit);
+    builder.setVertexMain(`
+gl_Position = vec4(aVertexPosition, 0.0, 1.0);
+`);
+    builder.setFragmentMain(`
+out_color = texelFetch(uSampler, ivec2(0, 0), 0);
+`);
+    return builder.build();
+  })());
+
+  private tempTextureArray = new Uint8Array(
+    [255, 255, 0, 255,]
+  )
+
+  // private pixelBuffer = this.registerDisposer(
+  //   getMemoizedBuffer(this.gl, WebGL2RenderingContext.PIXEL_UNPACK_BUFFER, () => this.tempTextureArray)).value;
+
   drawIndirect() {
-    console.log("drawIndirect for TransferFunctionPanel")
-    const {lineShader, gl, parent} = this;
+    console.log('drawIndirect for TransferFunctionPanel')
+    const {lineShader, gl, transferFunctionShader} = this;
     this.setGLLogicalViewport();
     gl.clearColor(0.0, 0.0, 0.0, 0.0);
     gl.clear(WebGL2RenderingContext.COLOR_BUFFER_BIT);
@@ -68,6 +94,21 @@ out_color = vec4(0.0, 1.0, 1.0, getLineAlpha());
     gl.blendFunc(WebGL2RenderingContext.SRC_ALPHA, WebGL2RenderingContext.ONE_MINUS_SRC_ALPHA);
     gl.disable(WebGL2RenderingContext.DEPTH_TEST);
     gl.disable(WebGL2RenderingContext.STENCIL_TEST);
+    {
+      transferFunctionShader.bind();
+      const aVertexPosition = transferFunctionShader.attribute('aVertexPosition');
+      this.cornersBuffer.bindToVertexAttrib(aVertexPosition, /*components=*/2, /*attributeType=*/WebGL2RenderingContext.FLOAT);
+      const textureUnit = transferFunctionShader.textureUnit(transferFunctionSamplerTextureUnit);
+
+      const texture = gl.createTexture();
+      gl.activeTexture(WebGL2RenderingContext.TEXTURE0 + textureUnit);
+      gl.bindTexture(WebGL2RenderingContext.TEXTURE_2D, texture);
+      setRawTextureParameters(gl);
+      gl.texImage2D(WebGL2RenderingContext.TEXTURE_2D, 0, WebGL2RenderingContext.RGBA, 1, 1, 0, WebGL2RenderingContext.RGBA, WebGL2RenderingContext.UNSIGNED_BYTE, this.tempTextureArray);
+      gl.drawArrays(WebGL2RenderingContext.TRIANGLE_FAN, 0, 4);
+      gl.disableVertexAttribArray(aVertexPosition);
+      gl.bindTexture(WebGL2RenderingContext.TEXTURE_2D, null);
+    }
     {
       const {renderViewport} = this;
       lineShader.bind();
