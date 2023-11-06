google/neuroglancer

commit 2c41c3402276cd540f23a5173bacc871156f7397

feat: render scale widget no longer out of bounds from JSON/Python

src/neuroglancer/image_user_layer.ts
@@ -34,7 +34,7 @@ import {setClipboard} from 'neuroglancer/util/clipboard';
 import {Borrowed} from 'neuroglancer/util/disposable';
 import {makeValueOrError} from 'neuroglancer/util/error';
 import {verifyOptionalObjectProperty} from 'neuroglancer/util/json';
-import {VOLUME_RENDERING_RESOLUTION_LOG_SCALE_ORIGIN, VolumeRenderingRenderLayer} from 'neuroglancer/volume_rendering/volume_render_layer';
+import {getVolumeRenderingDepthSamplesBounds, VOLUME_RENDERING_DEPTH_SAMPLES_DEFAULT_VALUE, VolumeRenderingRenderLayer} from 'neuroglancer/volume_rendering/volume_render_layer';
 import {makeWatchableShaderError, ParameterizedShaderGetterResult} from 'neuroglancer/webgl/dynamic_shader';
 import {setControlsInShader, ShaderControlsBuilderState, ShaderControlState} from 'neuroglancer/webgl/shader_ui_controls';
 import {ChannelDimensionsWidget} from 'neuroglancer/widget/channel_dimensions_widget';
@@ -59,13 +59,14 @@ const CROSS_SECTION_RENDER_SCALE_JSON_KEY = 'crossSectionRenderScale';
 const CHANNEL_DIMENSIONS_JSON_KEY = 'channelDimensions';
 const VOLUME_RENDERING_JSON_KEY = 'volumeRendering';
 const VOLUME_RENDERING_DEPTH_SAMPLES_KEY = 'volumeRenderingDepthSamples';
-const VOLUME_RENDERING_STARTING_LOG_SCALE = 6
 
 export interface ImageLayerSelectionState extends UserLayerSelectionState {
   value: any;
 }
 
 const Base = UserLayerWithAnnotationsMixin(UserLayer);
+const [volumeRenderingDepthSamplesOrigin, volumeRenderingDepthSamplesMax] =
+    getVolumeRenderingDepthSamplesBounds();
 export class ImageUserLayer extends Base {
   opacity = trackableAlphaValue(0.5);
   blendMode = trackableBlendModeValue();
@@ -73,11 +74,11 @@ export class ImageUserLayer extends Base {
   shaderError = makeWatchableShaderError();
   dataType = new WatchableValue<DataType|undefined>(undefined);
   sliceViewRenderScaleHistogram = new RenderScaleHistogram();
-  sliceViewRenderScaleTarget = trackableRenderScaleTarget(1);
-  volumeRenderingRenderScaleHistogram =
-      new RenderScaleHistogram(VOLUME_RENDERING_RESOLUTION_LOG_SCALE_ORIGIN);
-  volumeRenderingRenderScaleTarget =
-      trackableRenderScaleTarget(2 ** VOLUME_RENDERING_STARTING_LOG_SCALE);
+  sliceViewRenderScaleTarget = trackableRenderScaleTarget(1, );
+  volumeRenderingRenderScaleHistogram = new RenderScaleHistogram(volumeRenderingDepthSamplesOrigin);
+  volumeRenderingRenderScaleTarget = trackableRenderScaleTarget(
+      VOLUME_RENDERING_DEPTH_SAMPLES_DEFAULT_VALUE, volumeRenderingDepthSamplesOrigin,
+      volumeRenderingDepthSamplesMax);
 
   channelCoordinateSpace = new TrackableCoordinateSpace();
   channelCoordinateSpaceCombiner =

src/neuroglancer/render_scale_statistics.ts
@@ -15,9 +15,9 @@
  */
 
 import {TrackableValue} from 'neuroglancer/trackable_value';
-import {verifyFinitePositiveFloat} from 'neuroglancer/util/json';
-import {VisibilityPriorityAggregator} from 'neuroglancer/visibility_priority/frontend';
+import {makeVerifyNumberInInterval} from 'neuroglancer/util/json';
 import {NullarySignal} from 'neuroglancer/util/signal';
+import {VisibilityPriorityAggregator} from 'neuroglancer/visibility_priority/frontend';
 
 export const numRenderScaleHistogramBins = 40;
 export const renderScaleHistogramBinSize = 0.5;
@@ -33,8 +33,18 @@ export function getRenderScaleFromHistogramOffset(
   return 2 ** (offset * renderScaleHistogramBinSize + origin);
 }
 
-export function trackableRenderScaleTarget(initialValue: number) {
-  return new TrackableValue<number>(initialValue, verifyFinitePositiveFloat);
+export function trackableRenderScaleTarget(
+    initialValue: number, scaleOrigin: number = 2 ** renderScaleHistogramOrigin,
+    scaleMax?: number) {
+  if (scaleMax === undefined) {
+    scaleMax = 2 **
+            Math.round(
+                renderScaleHistogramBinSize * numRenderScaleHistogramBins +
+                renderScaleHistogramOrigin) -
+        1;
+  }
+  const verifyNumberInInterval = makeVerifyNumberInInterval(scaleOrigin, scaleMax);
+  return new TrackableValue<number>(initialValue, verifyNumberInInterval);
 }
 
 export class RenderScaleHistogram {

src/neuroglancer/volume_rendering/volume_render_layer.ts
@@ -39,7 +39,8 @@ import {addControlsToBuilder, setControlsInShader, ShaderControlsBuilderState, S
 import {defineVertexId, VertexIdHelper} from 'neuroglancer/webgl/vertex_id';
 import {clampToInterval} from 'src/neuroglancer/util/lerp';
 
-export const VOLUME_RENDERING_RESOLUTION_LOG_SCALE_ORIGIN = 1;
+export const VOLUME_RENDERING_DEPTH_SAMPLES_DEFAULT_VALUE = 64
+const VOLUME_RENDERING_DEPTH_SAMPLES_LOG_SCALE_ORIGIN = 1;
 const VOLUME_RENDERING_RESOLUTION_DEFAULT_BAR_HEIGHT = 10;
 
 interface TransformedVolumeSource extends
@@ -63,13 +64,15 @@ export interface VolumeRenderingRenderLayerOptions {
 const tempMat4 = mat4.create();
 const tempVisibleVolumetricClippingPlanes = new Float32Array(24);
 
-function clampAndRoundResolutionTargetValue(value: number) {
+export function getVolumeRenderingDepthSamplesBounds(): [number, number] {
   const logScaleMax = Math.round(
-      VOLUME_RENDERING_RESOLUTION_LOG_SCALE_ORIGIN +
+      VOLUME_RENDERING_DEPTH_SAMPLES_LOG_SCALE_ORIGIN +
       numRenderScaleHistogramBins * renderScaleHistogramBinSize);
-  return clampToInterval(
-             [2 ** VOLUME_RENDERING_RESOLUTION_LOG_SCALE_ORIGIN, 2 ** (logScaleMax - 1)],
-             Math.round(value)) as number;
+  return [2 ** VOLUME_RENDERING_DEPTH_SAMPLES_LOG_SCALE_ORIGIN, 2 ** logScaleMax - 1]
+}
+
+function clampAndRoundResolutionTargetValue(value: number) {
+  return clampToInterval(getVolumeRenderingDepthSamplesBounds(), Math.round(value)) as number;
 }
 
 export class VolumeRenderingRenderLayer extends PerspectiveViewRenderLayer {

src/neuroglancer/util/json.ts
@@ -52,6 +52,17 @@ export function verifyFinitePositiveFloat(obj: any): number {
   throw new Error(`Expected positive finite floating-point number, but received: ${x}.`);
 }
 
+export function makeVerifyNumberInInterval(minValue: number, maxValue: number) {
+  return (obj: any) => {
+    let x = verifyFloat(obj);
+    if (x >= minValue && x <= maxValue) {
+      return x;
+    }
+    throw new Error(
+        `Expected floating-point number in range [${minValue}, ${maxValue}], but received: ${x}.`);
+  };
+}
+
 export function parseXYZ<A extends WritableArrayLike<number>>(
     out: A, obj: any, validator: (x: any) => number = verifyFloat): A {
   verifyObject(obj);

commit bb596b27ae20754322d418e44895f21e58fa2eab

fix: bug introduced in 2c41c3402276 Had accidentally started the volume rendering scale at 4, not 2 Also remove accidental console.log

src/neuroglancer/image_user_layer.ts
@@ -34,7 +34,7 @@ import {setClipboard} from 'neuroglancer/util/clipboard';
 import {Borrowed} from 'neuroglancer/util/disposable';
 import {makeValueOrError} from 'neuroglancer/util/error';
 import {verifyOptionalObjectProperty} from 'neuroglancer/util/json';
-import {getVolumeRenderingDepthSamplesBounds, VOLUME_RENDERING_DEPTH_SAMPLES_DEFAULT_VALUE, VolumeRenderingRenderLayer} from 'neuroglancer/volume_rendering/volume_render_layer';
+import {getVolumeRenderingDepthSamplesBoundsLogScale, VOLUME_RENDERING_DEPTH_SAMPLES_DEFAULT_VALUE, VolumeRenderingRenderLayer} from 'neuroglancer/volume_rendering/volume_render_layer';
 import {makeWatchableShaderError, ParameterizedShaderGetterResult} from 'neuroglancer/webgl/dynamic_shader';
 import {setControlsInShader, ShaderControlsBuilderState, ShaderControlState} from 'neuroglancer/webgl/shader_ui_controls';
 import {ChannelDimensionsWidget} from 'neuroglancer/widget/channel_dimensions_widget';
@@ -65,8 +65,8 @@ export interface ImageLayerSelectionState extends UserLayerSelectionState {
 }
 
 const Base = UserLayerWithAnnotationsMixin(UserLayer);
-const [volumeRenderingDepthSamplesOrigin, volumeRenderingDepthSamplesMax] =
-    getVolumeRenderingDepthSamplesBounds();
+const [volumeRenderingDepthSamplesOriginLogScale, volumeRenderingDepthSamplesMaxLogScale] =
+    getVolumeRenderingDepthSamplesBoundsLogScale();
 export class ImageUserLayer extends Base {
   opacity = trackableAlphaValue(0.5);
   blendMode = trackableBlendModeValue();
@@ -76,10 +76,10 @@ export class ImageUserLayer extends Base {
   sliceViewRenderScaleHistogram = new RenderScaleHistogram();
   sliceViewRenderScaleTarget = trackableRenderScaleTarget(1);
   volumeRenderingChunkResolutionHistogram =
-      new RenderScaleHistogram(volumeRenderingDepthSamplesOrigin);
+      new RenderScaleHistogram(volumeRenderingDepthSamplesOriginLogScale);
   volumeRenderingDepthSamplesTarget = trackableRenderScaleTarget(
-      VOLUME_RENDERING_DEPTH_SAMPLES_DEFAULT_VALUE, volumeRenderingDepthSamplesOrigin,
-      volumeRenderingDepthSamplesMax);
+      VOLUME_RENDERING_DEPTH_SAMPLES_DEFAULT_VALUE, 2 ** volumeRenderingDepthSamplesOriginLogScale,
+      2 ** volumeRenderingDepthSamplesMaxLogScale - 1);
 
   channelCoordinateSpace = new TrackableCoordinateSpace();
   channelCoordinateSpaceCombiner =

src/neuroglancer/render_scale_statistics.ts
@@ -50,10 +50,10 @@ export function trackableRenderScaleTarget(
 export class RenderScaleHistogram {
   visibility = new VisibilityPriorityAggregator();
   changed = new NullarySignal();
-  origin: number;
+  logScaleOrigin: number;
 
   constructor(origin: number = renderScaleHistogramOrigin) {
-    this.origin = origin;
+    this.logScaleOrigin = origin;
   }
 
   /**
@@ -118,7 +118,7 @@ export class RenderScaleHistogram {
     }
     const index = spatialScaleIndex * numRenderScaleHistogramBins * 2 +
         Math.min(
-            Math.max(0, Math.round(getRenderScaleHistogramOffset(renderScale, this.origin))),
+            Math.max(0, Math.round(getRenderScaleHistogramOffset(renderScale, this.logScaleOrigin))),
             numRenderScaleHistogramBins - 1);
     value[index] += presentCount;
     value[index + numRenderScaleHistogramBins] += notPresentCount;

src/neuroglancer/volume_rendering/base.ts
@@ -113,7 +113,7 @@ export function forEachVisibleVolumeRenderingChunk<
     const physicalSpacing = Math.cbrt(viewVolume * canonicalToPhysicalScale / viewDet);
     const optimalSamples = depthRange / Math.cbrt(viewVolume);
     histogramInformation.spatialScales.set(physicalSpacing, optimalSamples);
-    if ((viewVolume - targetViewVolume) > 0) {
+    if ((viewVolume - targetViewVolume) >= 0) {
       bestViewVolume = viewVolume;
       bestScaleIndex = scaleIndex;
     }

src/neuroglancer/volume_rendering/volume_render_layer.ts
@@ -64,15 +64,18 @@ export interface VolumeRenderingRenderLayerOptions {
 const tempMat4 = mat4.create();
 const tempVisibleVolumetricClippingPlanes = new Float32Array(24);
 
-export function getVolumeRenderingDepthSamplesBounds(): [number, number] {
+export function getVolumeRenderingDepthSamplesBoundsLogScale(): [number, number] {
   const logScaleMax = Math.round(
       VOLUME_RENDERING_DEPTH_SAMPLES_LOG_SCALE_ORIGIN +
       numRenderScaleHistogramBins * renderScaleHistogramBinSize);
-  return [2 ** VOLUME_RENDERING_DEPTH_SAMPLES_LOG_SCALE_ORIGIN, 2 ** logScaleMax - 1]
+  return [VOLUME_RENDERING_DEPTH_SAMPLES_LOG_SCALE_ORIGIN, logScaleMax]
 }
 
 function clampAndRoundResolutionTargetValue(value: number) {
-  return clampToInterval(getVolumeRenderingDepthSamplesBounds(), Math.round(value)) as number;
+  const logScaleDepthSamplesBounds = getVolumeRenderingDepthSamplesBoundsLogScale();
+  const depthSamplesBounds: [number, number] =
+      [2 ** logScaleDepthSamplesBounds[0], 2 ** logScaleDepthSamplesBounds[1] - 1];
+  return clampToInterval(depthSamplesBounds, Math.round(value)) as number;
 }
 
 export class VolumeRenderingRenderLayer extends PerspectiveViewRenderLayer {
@@ -335,7 +338,6 @@ void main() {
                 VOLUME_RENDERING_RESOLUTION_INDICATOR_BAR_HEIGHT, true)
             alreadyStoredSamples.add(roundedSamples);
           }
-          console.log(alreadyStoredSamples);
           index--;
         });
         renderScaleHistogram.add(