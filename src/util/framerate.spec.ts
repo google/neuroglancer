43ed997bba50cbc9ad6e67d4e92c2b44ade92063
Sean Martin <martins7@tcd.ie>
2024-04-30 11:16:34 +0200

    feat: framerate counter

---
+++ src/util/framerate.spec.ts
@@ -0,0 +1,46 @@
+/**
+ * @license
+ * Copyright 2024 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { describe, it, expect } from "vitest";
+import { FrameRateCounter } from "#src/util/framerate.js";
+
+describe("FrameRateCounter", () => {
+  it("calculates valid fps for evenly spaced frames", () => {
+    const frameRateCounter = new FrameRateCounter(10);
+    for (let i = 0; i < 10; i++) {
+      frameRateCounter.addFrame(i * 100);
+      if (i === 0) {
+        expect(frameRateCounter.calculateFrameTimeInMs()).toEqual(0);
+      } else {
+        expect(frameRateCounter.calculateFrameTimeInMs()).toEqual(100);
+      }
+    }
+  });
+  it("calculates valid fps for many frames", () => {
+    const frameRateCounter = new FrameRateCounter(10);
+    for (let i = 0; i < 10; i++) {
+      frameRateCounter.addFrame(i * 100);
+    }
+    for (let i = 0; i < 10; i++) {
+      frameRateCounter.addFrame(i * 10);
+    }
+    expect(frameRateCounter.calculateFrameTimeInMs()).toEqual(10);
+  });
+  it("removes frames if no frames have been requested", () => {
+    // TODO (SKM) implement this test
+    expect(new FrameRateCounter().calculateFrameTimeInMs()).toEqual(0);
+  });
+});
---
+++ src/util/framerate.ts
@@ -0,0 +1,56 @@
+/**
+ * @license
+ * Copyright 2024 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export class FrameRateCounter {
+  private frameTimeStamps: number[] = [];
+  constructor(
+    private numberOfStoredFrameTimes: number = 10,
+    private timeoutInMS: number = 1000,
+  ) {}
+
+  start(timestamp: number = Date.now()) {
+    if (this.frameTimeStamps.length == 0) {
+      this.frameTimeStamps.push(timestamp);
+    } else if (
+      timestamp - this.frameTimeStamps[this.frameTimeStamps.length - 1] >=
+      this.timeoutInMS
+    ) {
+      this.reset();
+      this.frameTimeStamps.push(timestamp);
+    }
+  }
+  reset() {
+    this.frameTimeStamps = [];
+  }
+
+  addFrame(timestamp: number = Date.now()) {
+    if (this.frameTimeStamps.length >= this.numberOfStoredFrameTimes) {
+      this.frameTimeStamps.shift();
+    }
+    this.frameTimeStamps.push(timestamp);
+  }
+
+  calculateFrameTimeInMs() {
+    if (this.frameTimeStamps.length <= 1) {
+      return 0;
+    }
+    return (
+      (this.frameTimeStamps[this.frameTimeStamps.length - 1] -
+        this.frameTimeStamps[0]) /
+      (this.frameTimeStamps.length - 1)
+    );
+  }
+}
