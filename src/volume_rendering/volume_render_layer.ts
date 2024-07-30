032358e45a3a21c2bdbce2ab896a4126b3ef113f
Sean Martin <martins7@tcd.ie>
2024-07-30 15:04:34 +0200

    fix: remove discard in 3D histogram for display purposes this means the histogram now reflects the underlying data more

--- src/volume_rendering/volume_render_layer.ts
+++ src/volume_rendering/volume_render_layer.ts
@@ -628,15 +628,10 @@ float getHistogramValue${i}() {
     simpleFloatHash(vec2(aInput1 + float(gl_VertexID) + 20.0, 15.0 + float(gl_InstanceID))));
   chunkSamplePosition = rand3val * (uChunkDataSize - 1.0);
 ${histogramFetchCode}
-  if (x == 0.0) {
-    gl_Position = vec4(2.0, 2.0, 2.0, 1.0);
-  }
-  else {
-    if (x < 0.0) x = 0.0;
-    else if (x > 1.0) x = 1.0;
-    else x = (1.0 + x * 253.0) / 255.0;
-    gl_Position = vec4(2.0 * (x * 255.0 + 0.5) / 256.0 - 1.0, 0.0, 0.0, 1.0);
-  }
+  if (x < 0.0) x = 0.0;
+  else if (x > 1.0) x = 1.0;
+  else x = (1.0 + x * 253.0) / 255.0;
+  gl_Position = vec4(2.0 * (x * 255.0 + 0.5) / 256.0 - 1.0, 0.0, 0.0, 1.0);
   gl_PointSize = 1.0;`);
           builder.setFragmentMain(`
 outputValue = vec4(1.0, 1.0, 1.0, 1.0);
