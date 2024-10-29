27a2a70fa9dbc6bffbb4951c9345e51a85e88bdb
Rafael Vescovi <ravescovi@gmail.com>
2024-10-28 22:27:37 -0500

    start endpoint configuration

--- src/datasource/globusauth/credentials_provider.ts
+++ src/datasource/globusauth/credentials_provider.ts
@@ -58,15 +58,27 @@ async function waitForLogin(serverUrl: string): Promise<GlobusAuthToken> {
 
   const res: Promise<GlobusAuthToken> = new Promise((f) => {
     function writeLoginStatus(message: string, buttonMessage: string) {
-      status.element.textContent = message + " ";
+
       const login_button = document.createElement("button");
       const submit_button = document.createElement("button");
+      const ep_text = document.createElement("textarea");
       const token_text = document.createElement("textarea");
+
       login_button.textContent = buttonMessage;
       submit_button.textContent = "Submit token";
+
+      status.element.textContent = message;
+      status.element.appendChild(document.createElement("br"));
+      status.element.textContent = "Endpoint ID: ";
+      status.element.appendChild(ep_text);
+      status.element.appendChild(document.createElement("br"));
+      
+      status.element.appendChild(document.createElement("br"));
       status.element.appendChild(login_button);
       status.element.appendChild(submit_button);
       status.element.appendChild(token_text);
+      
+      
       
 
       login_button.addEventListener("click", async () => {
@@ -76,9 +88,11 @@ async function waitForLogin(serverUrl: string): Promise<GlobusAuthToken> {
           "Retry",
         );
 
-        // const collection_scope = 'https://auth.globus.org/scopes/aa12ebd6-d442-47ab-b630-54e18044c8bd/https'
-        const collection_scope = 'https://auth.globus.org/scopes/05d2c76a-e867-4f67-aa57-76edeb0beda0/data_access+https://auth.globus.org/scopes/05d2c76a-e867-4f67-aa57-76edeb0beda0/https'
-        
+        let ep_id = ep_text.value
+        if (ep_id === '') {
+          ep_id = "05d2c76a-e867-4f67-aa57-76edeb0beda0";
+          }
+        const collection_scope = `https://auth.globus.org/scopes/${ep_id}/data_access+https://auth.globus.org/scopes/${ep_id}/https`
 
         openPopupCenter(
           `https://auth.globus.org/v2/oauth2/authorize?scope=${collection_scope}&code_challenge=${code_challenge.code_challenge}&code_challenge_method=S256&redirect_uri=https%3A%2F%2Fauth.globus.org%2Fv2%2Fweb%2Fauth-code&response_type=code&client_id=${client_id}`,
@@ -88,7 +102,7 @@ async function waitForLogin(serverUrl: string): Promise<GlobusAuthToken> {
       submit_button.addEventListener("click", async () => {
         console.log('Submit clicked')
             const accessCode = token_text.value
-  
+            
 
             const response = await fetch(`https://auth.globus.org/v2/oauth2/token?grant_type=authorization_code&code=${accessCode}&redirect_uri=https%3A%2F%2Fauth.globus.org%2Fv2%2Fweb%2Fauth-code&code_verifier=${code_challenge.code_verifier}&client_id=${client_id}`, {
               method: 'POST',
@@ -149,7 +163,6 @@ export class GlobusAuthCredentialsProvider extends CredentialsProvider<GlobusAut
       saveAuthTokenToLocalStorage(this.serverUrl, token);
     }
 
-    // token = await waitForLogin(this.serverUrl);
     return token;
   });
 }
