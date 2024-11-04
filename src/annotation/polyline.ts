40d2d8a1c748407aff50c36c382038f16bf1dd7e
Sean Martin <martins7@tcd.ie>
2024-11-04 12:59:57 +0100

    fix: less barebones polyline render

--- src/annotation/polyline.ts
+++ src/annotation/polyline.ts
@@ -50,17 +50,17 @@ const PICK_IDS_PER_INSTANCE = ENDPOINTS_PICK_OFFSET + 2;
 
 function defineNoOpEndpointMarkerSetters(builder: ShaderBuilder) {
   builder.addVertexCode(`
-void setEndpointMarkerSize(float startSize, float endSize) {}
-void setEndpointMarkerBorderWidth(float startSize, float endSize) {}
-void setEndpointMarkerColor(vec4 startColor, vec4 endColor) {}
-void setEndpointMarkerBorderColor(vec4 startColor, vec4 endColor) {}
+void setPEndpointMarkerSize(float startSize, float endSize) {}
+void setPEndpointMarkerBorderWidth(float startSize, float endSize) {}
+void setPEndpointMarkerColor(vec4 startColor, vec4 endColor) {}
+void setPEndpointMarkerBorderColor(vec4 startColor, vec4 endColor) {}
 `);
 }
 
 function defineNoOpLineSetters(builder: ShaderBuilder) {
   builder.addVertexCode(`
-void setLineWidth(float width) {}
-void setLineColor(vec4 startColor, vec4 endColor) {}
+void setPLineWidth(float width) {}
+void setPLineColor(vec4 startColor, vec4 endColor) {}
 `);
 }
 
@@ -80,20 +80,20 @@ class RenderHelper extends AnnotationRenderHelper {
     );
   }
 
-    private vertexIdHelper = this.registerDisposer(VertexIdHelper.get(this.gl));
+  private vertexIdHelper = this.registerDisposer(VertexIdHelper.get(this.gl));
 
-    private edgeShaderGetter = this.getDependentShader(
-      "annotation/polyline/edge",
-      (builder: ShaderBuilder) => {
-        const { rank } = this;
-        this.defineShader(builder);
-        defineLineShader(builder);
-        builder.addVarying(`highp float[${rank}]`, "vModelPosition");
-        builder.addVertexCode(`
+  private edgeShaderGetter = this.getDependentShader(
+    "annotation/polyline/edge",
+    (builder: ShaderBuilder) => {
+      const { rank } = this;
+      this.defineShader(builder);
+      defineLineShader(builder);
+      builder.addVarying(`highp float[${rank}]`, "vModelPosition");
+      builder.addVertexCode(`
   float ng_LineWidth;
   `);
-        defineNoOpEndpointMarkerSetters(builder);
-        builder.addVertexCode(`
+      defineNoOpEndpointMarkerSetters(builder);
+      builder.addVertexCode(`
   void setLineWidth(float width) {
     ng_LineWidth = width;
   }
@@ -101,7 +101,7 @@ class RenderHelper extends AnnotationRenderHelper {
     vColor = mix(startColor, endColor, getLineEndpointCoefficient());
   }
   `);
-        builder.setVertexMain(`
+      builder.setVertexMain(`
   float modelPositionA[${rank}] = getVertexPosition0();
   float modelPositionB[${rank}] = getVertexPosition1();
   for (int i = 0; i < ${rank}; ++i) {
@@ -115,25 +115,25 @@ class RenderHelper extends AnnotationRenderHelper {
            ng_LineWidth);
   ${this.setPartIndex(builder)};
   `);
-        builder.setFragmentMain(`
+      builder.setFragmentMain(`
   float clipCoefficient = getSubspaceClipCoefficient(vModelPosition);
   emitAnnotation(vec4(vColor.rgb, vColor.a * getLineAlpha() *
                                   ${this.getCrossSectionFadeFactor()} *
                                   clipCoefficient));
   `);
-      },
-    );
+    },
+  );
 
-    private endpointShaderGetter = this.getDependentShader(
-      "annotation/polyline/endpoint",
-      (builder: ShaderBuilder) => {
-        const { rank } = this;
-        this.defineShader(builder);
-        defineCircleShader(builder, this.targetIsSliceView);
-        builder.addVarying("highp float", "vClipCoefficient");
-        builder.addVarying("highp vec4", "vBorderColor");
-        defineNoOpLineSetters(builder);
-        builder.addVertexCode(`
+  private endpointShaderGetter = this.getDependentShader(
+    "annotation/polyline/endpoint",
+    (builder: ShaderBuilder) => {
+      const { rank } = this;
+      this.defineShader(builder);
+      defineCircleShader(builder, this.targetIsSliceView);
+      builder.addVarying("highp float", "vClipCoefficient");
+      builder.addVarying("highp vec4", "vBorderColor");
+      defineNoOpLineSetters(builder);
+      builder.addVertexCode(`
   float ng_markerDiameter;
   float ng_markerBorderWidth;
   int getEndpointIndex() {
@@ -152,7 +152,7 @@ class RenderHelper extends AnnotationRenderHelper {
     vBorderColor = mix(startColor, endColor, float(getEndpointIndex()));
   }
   `);
-        builder.setVertexMain(`
+      builder.setVertexMain(`
   float modelPosition[${rank}] = getVertexPosition0();
   float modelPositionB[${rank}] = getVertexPosition1();
   for (int i = 0; i < ${rank}; ++i) {
@@ -167,56 +167,56 @@ class RenderHelper extends AnnotationRenderHelper {
   emitCircle(uModelViewProjection * vec4(projectModelVectorToSubspace(modelPosition), 1.0), ng_markerDiameter, ng_markerBorderWidth);
   ${this.setPartIndex(builder, "uint(getEndpointIndex()) + 1u")};
   `);
-        builder.setFragmentMain(`
+      builder.setFragmentMain(`
   vec4 color = getCircleColor(vColor, vBorderColor);
   color.a *= vClipCoefficient;
   emitAnnotation(color);
   `);
-      },
-    );
+    },
+  );
 
-    enable(
-      shaderGetter: AnnotationShaderGetter,
-      context: AnnotationRenderContext,
-      callback: (shader: ShaderProgram) => void,
-    ) {
-      super.enable(shaderGetter, context, (shader) => {
-        const binder = shader.vertexShaderInputBinders.VertexPosition;
-        binder.enable(1);
-        this.gl.bindBuffer(
-          WebGL2RenderingContext.ARRAY_BUFFER,
-          context.buffer.buffer,
-        );
-        binder.bind(this.geometryDataStride, context.bufferOffset);
-        const { vertexIdHelper } = this;
-        vertexIdHelper.enable();
-        callback(shader);
-        vertexIdHelper.disable();
-        binder.disable();
-      });
-    }
+  enable(
+    shaderGetter: AnnotationShaderGetter,
+    context: AnnotationRenderContext,
+    callback: (shader: ShaderProgram) => void,
+  ) {
+    super.enable(shaderGetter, context, (shader) => {
+      const binder = shader.vertexShaderInputBinders.VertexPosition;
+      binder.enable(1);
+      this.gl.bindBuffer(
+        WebGL2RenderingContext.ARRAY_BUFFER,
+        context.buffer.buffer,
+      );
+      binder.bind(this.geometryDataStride, context.bufferOffset);
+      const { vertexIdHelper } = this;
+      vertexIdHelper.enable();
+      callback(shader);
+      vertexIdHelper.disable();
+      binder.disable();
+    });
+  }
 
-    drawEdges(context: AnnotationRenderContext) {
-      this.enable(this.edgeShaderGetter, context, (shader) => {
-        initializeLineShader(
-          shader,
-          context.renderContext.projectionParameters,
-          /*featherWidthInPixels=*/ 1.0,
-        );
-        drawLines(shader.gl, 1, context.count);
-      });
-    }
+  drawEdges(context: AnnotationRenderContext) {
+    this.enable(this.edgeShaderGetter, context, (shader) => {
+      initializeLineShader(
+        shader,
+        context.renderContext.projectionParameters,
+        /*featherWidthInPixels=*/ 1.0,
+      );
+      drawLines(shader.gl, 1, context.count);
+    });
+  }
 
-    drawEndpoints(context: AnnotationRenderContext) {
-      this.enable(this.endpointShaderGetter, context, (shader) => {
-        initializeCircleShader(
-          shader,
-          context.renderContext.projectionParameters,
-          { featherWidthInPixels: 0.5 },
-        );
-        drawCircles(shader.gl, 2, context.count);
-      });
-    }
+  drawEndpoints(context: AnnotationRenderContext) {
+    this.enable(this.endpointShaderGetter, context, (shader) => {
+      initializeCircleShader(
+        shader,
+        context.renderContext.projectionParameters,
+        { featherWidthInPixels: 0.5 },
+      );
+      drawCircles(shader.gl, 2, context.count);
+    });
+  }
 
   draw(context: AnnotationRenderContext) {
     this.drawEdges(context);
@@ -249,10 +249,17 @@ function snapPositionToEndpoint(
 registerAnnotationTypeRenderHandler<Polyline>(AnnotationType.POLYLINE, {
   sliceViewRenderHelper: RenderHelper,
   perspectiveViewRenderHelper: RenderHelper,
-  defineShaderNoOpSetters(builder) {},
+  defineShaderNoOpSetters(builder) {
+    defineNoOpEndpointMarkerSetters(builder);
+    defineNoOpLineSetters(builder);
+  },
   pickIdsPerInstance: 0,
-  snapPosition(position, data, offset, partIndex) {},
-  getRepresentativePoint(out, ann, partIndex) {},
+  snapPosition(position, data, offset, partIndex) {
+    // TODO (skm) See line for reference, but needs to pick the correct line
+  },
+  getRepresentativePoint(out, ann, partIndex) {
+    // TODO (skm) See line for reference
+  },
   updateViaRepresentativePoint(oldAnnotation, position, partIndex) {
     return oldAnnotation;
   },
