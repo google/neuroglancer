b2945ff71dd50a1f6a792b757c86f9086dd4885a
aranega <vincent.aranega@gmail.com>
2024-11-15 11:04:39 -0600

    Fix removeJsonString not finding some keybindings
    When a key is inserted in the localToolBinder, the "toJSON()" method is
    called on the tool. The JSON retrieved from this call is stringyfied and
    used as key. The "toJSON()" method acts differently on Tool and
    LegacyTool. When a LayerTool (inheriting from Tool) is created, the
    "jsonId" is returned by "toJSON()", the strinfigication of this value
    leads then to insert in the localToolBinder map "mykey" (with quotes) instead of
    simply mykey. This result in the need to explicitally use
    "JSON.stringify(...)" on a key before calling "removeJsonString(...)", which
    is not ideal.
    This patch on "removeJsonString" searches first for the key and
    fallsback on the "JSON.stringify(...)" if the key cannot be found in the
    first place.

--- src/ui/tool.ts
+++ src/ui/tool.ts
@@ -443,7 +443,7 @@ export class LocalToolBinder<
   }
 
   removeJsonString(toolJsonString: string) {
-    const key = this.jsonToKey.get(toolJsonString);
+    const key = this.jsonToKey.get(toolJsonString) || this.jsonToKey.get(JSON.stringify(toolJsonString));
     if (key === undefined) return;
     this.set(key, undefined);
   }
