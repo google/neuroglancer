cd3b721892f01a9a80b3407d543312c477c3a82a
Josh Moore <josh@openmicroscopy.org>
2024-11-21 11:09:28 +0100

    Also update frontend

--- src/datasource/zarr/frontend.ts
+++ src/datasource/zarr/frontend.ts
@@ -501,7 +501,7 @@ export class ZarrDataSource extends DataSourceProvider {
         let multiscaleInfo: ZarrMultiscaleInfo;
         if (metadata.nodeType === "group") {
           // May be an OME-zarr multiscale dataset.
-          const multiscale = parseOmeMetadata(url, metadata.userAttributes, this.zarrVersion);
+          const multiscale = parseOmeMetadata(url, metadata.userAttributes, metadata.zarrVersion);
           if (multiscale === undefined) {
             throw new Error("Neithre array nor OME multiscale metadata found");
           }
