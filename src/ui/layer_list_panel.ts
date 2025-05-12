fb4b89e60c6e3a8e9fba51bfcfde9a1846fbee36
Sean Martin <martins7@tcd.ie>
2025-05-12 12:45:53 +0200

    fix: don't double multi setup listeners

--- src/ui/layer_list_panel.ts
+++ src/ui/layer_list_panel.ts
@@ -165,6 +165,7 @@ class LayerColorWidget extends RefCounted {
             updateLayerColorWidget();
           }),
         );
+        this.isListeningForColorChange = true;
       }),
     );
     this.registerDisposer(
