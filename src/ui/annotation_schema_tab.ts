42de28a94b14c96c5cbc0cfe9ec04fd57cb33eab
Sean Martin <martins7@tcd.ie>
2025-06-09 18:13:58 +0200

    chore: add license

--- src/ui/annotation_schema_tab.ts
+++ src/ui/annotation_schema_tab.ts
@@ -1,3 +1,23 @@
+/**
+ * @license
+ * Copyright 2025 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file User interface for viewing and editing the annotation schema.
+ */
+
 import svg_numbers from "ikonate/icons/hash.svg?raw";
 import svg_palette from "ikonate/icons/drop.svg?raw";
 import svg_check from "ikonate/icons/ok-circle.svg?raw";
@@ -531,6 +551,7 @@ export class AnnotationSchemaView extends Tab {
     // TODO (SKM) - again may need to unregister the event listeners
     inputs.forEach((input) => {
       container.appendChild(input);
+      // TODO We should refactor isMutable to instead be the name "readonly"
       if (this.isMutable.value) {
         input.addEventListener("change", changeFunction);
       }
