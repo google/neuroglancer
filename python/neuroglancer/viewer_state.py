90c8fb6d377d68f4530286185257877b3b82a884
Casey Schneider-Mizell <caseysm@gmail.com>
2025-06-25 13:33:19 -0700

    add swap_visible_segments to AnnotationLayer

--- python/neuroglancer/viewer_state.py
+++ python/neuroglancer/viewer_state.py
@@ -1164,6 +1164,9 @@ class AnnotationLayer(Layer, _AnnotationLayerOptions):
     shader_controls = shaderControls = wrapped_property(
         "shaderControls", ShaderControls
     )
+    swap_visible_segments_on_move = swapVisbleSegmentsOnMove = wrapped_property(
+        "swapVisibleSegmentsOnMove", optional(bool, True)
+    )
 
     @staticmethod
     def interpolate(a, b, t):
