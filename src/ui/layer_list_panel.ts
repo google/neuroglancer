bcb2e6683e2e7bb90dc5a973fb3c4d8429ce9be0
Sean Martin <martins7@tcd.ie>
2025-07-08 13:23:07 +0200

    fix(ui): correct tool palette checkbox title Also clarify intended behaviour of message. Current was probably mixed up because enableTitle could be the title when enabled or the message shown to indicate how to enable

--- src/ui/layer_list_panel.ts
+++ src/ui/layer_list_panel.ts
@@ -163,8 +163,8 @@ class LayerListItem extends RefCounted {
             changed: layer.layerChanged,
           },
           {
-            enableTitle: "Archive layer (disable and remove from layer groups)",
-            disableTitle:
+            enabledTitle: "Archive layer (disable and remove from layer groups)",
+            disabledTitle:
               "Unarchive layer (enable and add to all layer groups)",
           },
         ),
