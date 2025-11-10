b2cc38d9da01bc3fca2e49bf02448a52ebf5d560
brieuc.crosson <crosson.brieuc@gmail.com>
2025-11-10 11:06:45 +0100

    feat: add dynamic cursor for voxel picker tool

--- src/ui/voxel_annotations.ts
+++ src/ui/voxel_annotations.ts
@@ -398,9 +398,32 @@ export class VoxelFloodFillTool extends BaseVoxelTool {
   }
 }
 
+const pickerSVG = `<svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+<path d="M7 13.161L12.4644 7.6966C12.8549 7.30607 13.4881 7.30607 13.8786 7.6966L15.9999 9.81792C16.3904 10.2084 16.3904 10.8416 15.9999 11.2321L14.0711 13.161M7 13.161L4.82764 15.3334C4.73428 15.4267 4.66034 15.5376 4.61007 15.6597L3.58204 18.1563C3.07438 19.3892 4.30728 20.6221 5.54018 20.1145L8.03681 19.0865C8.1589 19.0362 8.26981 18.9622 8.36317 18.8689L14.0711 13.161M7 13.161H14.0711" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+<path d="M13.878 3.45401L15.9993 5.57533M20.242 9.81798L18.1206 7.69666M15.9993 5.57533L17.4135 4.16112C17.8041 3.7706 18.4372 3.7706 18.8277 4.16112L19.5349 4.86823C19.9254 5.25875 19.9254 5.89192 19.5349 6.28244L18.1206 7.69666M15.9993 5.57533L18.1206 7.69666" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+
+<path d="M7 13.161L12.4644 7.6966C12.8549 7.30607 13.4881 7.30607 13.8786 7.6966L15.9999 9.81792C16.3904 10.2084 16.3904 10.8416 15.9999 11.2321L14.0711 13.161M7 13.161L4.82764 15.3334C4.73428 15.4267 4.66034 15.5376 4.61007 15.6597L3.58204 18.1563C3.07438 19.3892 4.30728 20.6221 5.54018 20.1145L8.03681 19.0865C8.1589 19.0362 8.26981 18.9622 8.36317 18.8689L14.0711 13.161M7 13.161H14.0711" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+<path d="M13.878 3.45401L15.9993 5.57533M20.242 9.81798L18.1206 7.69666M15.9993 5.57533L17.4135 4.16112C17.8041 3.7706 18.4372 3.7706 18.8277 4.16112L19.5349 4.86823C19.9254 5.25875 19.9254 5.89192 19.5349 6.28244L18.1206 7.69666M15.9993 5.57533L18.1206 7.69666" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>`;
+
+const pickerCursor = `url('data:image/svg+xml;utf8,${encodeURIComponent(pickerSVG)}') 4 19, crosshair`;
+
+
 export class AdoptVoxelLabelTool extends LayerTool<UserLayerWithVoxelEditing> {
   constructor(layer: UserLayerWithVoxelEditing) {
     super(layer, /*toggle=*/ false);
+  }
+
+  protected setCursor(cursor: string) {
+    for (const panel of this.layer.manager.root.display.panels) {
+      panel.element.style.setProperty("cursor", cursor, "important");
+    }
+  }
+
+  protected resetCursor() {
+    for (const panel of this.layer.manager.root.display.panels) {
+      panel.element.style.removeProperty("cursor");
+    }
   }
 
   toJSON() {
@@ -411,8 +434,10 @@ export class AdoptVoxelLabelTool extends LayerTool<UserLayerWithVoxelEditing> {
     return "Picking tool";
   }
 
-  activate(_activation: ToolActivation<this>): void {
+  activate(activation: ToolActivation<this>): void {
     if (!this.mouseState?.active) return;
+    this.setCursor(pickerCursor);
+    activation.registerDisposer(() => {this.resetCursor()})
 
     const voxelEditingContext = this.layer.editingContexts.values().next().value as VoxelEditingContext;
     if (!voxelEditingContext) {
