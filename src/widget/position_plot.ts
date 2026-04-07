a59caae83ff6820acf793e27fc9b78d75983f684
Sean Martin <seankieran.m@gmail.com>
2026-04-07 13:55:01 +0200

    feat(ui): allow position plots to only show min/max bounds via uiConfiguration (#792)
    * feat: force only show min max bounds in position widget needs ui config
    
    * feat: allow main dim plot to only have min/max bounds
    
    * feat: layer group viewer doesn't show bounds
    
    * chore: lint
    
    * feat: allow plot bounds to be watched
    
    * feat: support changing the dim plot bounds for layer groups
    
    * refactor: simplify min/max check a little
    
    * chore: lint
    
    * feat(python): add option to python viewer config for plot dim bounds
    
    * refactor: simplify normalized bound setting as @chrisj suggested

--- src/widget/position_plot.ts
+++ src/widget/position_plot.ts
@@ -25,6 +25,7 @@ import {
   getDisplayLowerUpperBounds,
 } from "#src/coordinate_transform.js";
 import type { Position } from "#src/navigation_state.js";
+import type { WatchableValueInterface } from "#src/trackable_value.js";
 import { WatchableValue } from "#src/trackable_value.js";
 import { animationFrameDebounce } from "#src/util/animation_frame_debounce.js";
 import { filterArrayInplace } from "#src/util/array.js";
@@ -106,6 +107,7 @@ export class PositionPlot extends RefCounted {
   constructor(
     public position: Position,
     public dimensionId: DimensionId,
+    private showAllBounds?: WatchableValueInterface<boolean>,
     public orientation: "row" | "column" = "column",
   ) {
     super();
@@ -176,6 +178,15 @@ export class PositionPlot extends RefCounted {
         this.element.style.display = "none";
         this.visible = false;
         return;
+      }
+      if (this.showAllBounds?.value === false) {
+        const { normalizedBounds } = normalizedDimensionBounds;
+        normalizedDimensionBounds.normalizedBounds = [
+          {
+            lower: Math.min(...normalizedBounds.map((b) => b.lower)),
+            upper: Math.max(...normalizedBounds.map((b) => b.upper)),
+          },
+        ];
       }
       this.element.style.display = "";
       this.visible = true;
@@ -282,6 +293,9 @@ export class PositionPlot extends RefCounted {
       animationFrameDebounce(updateView),
     );
     this.registerDisposer(this.position.changed.add(scheduleUpdateView));
+    if (this.showAllBounds !== undefined) {
+      this.registerDisposer(this.showAllBounds.changed.add(scheduleUpdateView));
+    }
     const getPositionFromMouseEvent = (
       event: MouseEvent,
     ): number | undefined => {
