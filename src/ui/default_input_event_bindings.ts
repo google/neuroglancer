ccd81ec5e6bc39504f545d7ed1e8d472c0c4a32b
Forrest Collman <forrest.collman@gmail.com>
2026-07-24 23:28:07 -0700

    remove blank

--- src/ui/default_input_event_bindings.ts
+++ src/ui/default_input_event_bindings.ts
@@ -48,7 +48,6 @@ export function getDefaultGlobalBindings() {
     map.set("space", "toggle-layout");
     map.set("shift+space", "toggle-layout-alternative");
     map.set("backslash", "toggle-show-statistics");
-
     defaultGlobalBindings = map;
   }
   return defaultGlobalBindings;
