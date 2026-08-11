29a53fb8cfbdb074cfa86b55c2a5f8ea7b437107
William Silversmith <william.silversmith@gmail.com>
2026-08-11 15:13:24 -0400

    docs: fix copyright notice Silvermsith -> Silversmith

--- src/sliceview/crackle/src/pins.hpp
+++ src/sliceview/crackle/src/pins.hpp
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2026 William Silvermsith
+ * Copyright 2026 William Silversmith
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
